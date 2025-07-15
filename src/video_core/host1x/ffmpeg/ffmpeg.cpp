53ffdbcfb34c1e4c5430c1b9ff2176e3d5701987
MaranBr <maranbr@outlook.com>
2025-07-14 20:57:24 -0400

    Update the preferred GPU decoder list for each platform

--- src/video_core/host1x/ffmpeg/ffmpeg.cpp
+++ src/video_core/host1x/ffmpeg/ffmpeg.cpp
@@ -26,14 +26,16 @@ namespace {
 constexpr AVPixelFormat PreferredGpuFormat = AV_PIX_FMT_NV12;
 constexpr AVPixelFormat PreferredCpuFormat = AV_PIX_FMT_YUV420P;
 constexpr std::array PreferredGpuDecoders = {
-#ifdef _WIN32
+#if defined (_WIN32)
     AV_HWDEVICE_TYPE_CUDA,
     AV_HWDEVICE_TYPE_D3D11VA,
     AV_HWDEVICE_TYPE_DXVA2,
 #elif defined(__FreeBSD__)
     AV_HWDEVICE_TYPE_VDPAU,
 #elif defined(__unix__)
+	AV_HWDEVICE_TYPE_CUDA,
     AV_HWDEVICE_TYPE_VAAPI,
+	AV_HWDEVICE_TYPE_VDPAU,
 #endif
     AV_HWDEVICE_TYPE_VULKAN,
 };
