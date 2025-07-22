35082084731f802326100129e810954131001428
crueter <crueter@eden-emu.dev>
2025-07-22 17:12:24 -0400

    Fix license headers
    Signed-off-by: crueter <crueter@eden-emu.dev>

--- .ci/license-header.sh
+++ .ci/license-header.sh
@@ -5,13 +5,19 @@ HEADER_HASH="$(cat "$PWD/.ci/license/header-hash.txt")"
 
 echo "Getting branch changes"
 
-BRANCH=`git rev-parse --abbrev-ref HEAD`
-COMMITS=`git log ${BRANCH} --not master --pretty=format:"%h"`
-RANGE="${COMMITS[${#COMMITS[@]}-1]}^..${COMMITS[0]}"
+# BRANCH=`git rev-parse --abbrev-ref HEAD`
+# COMMITS=`git log ${BRANCH} --not master --pretty=format:"%h"`
+# RANGE="${COMMITS[${#COMMITS[@]}-1]}^..${COMMITS[0]}"
+# FILES=`git diff-tree --no-commit-id --name-only ${RANGE} -r`
+
+CURRENT=`git rev-parse --short=10 HEAD`
+BASE=`git merge-base master $CURRENT`
+RANGE="$CURRENT^..$BASE"
 FILES=`git diff-tree --no-commit-id --name-only ${RANGE} -r`
 
 #FILES=$(git diff --name-only master)
 
+echo $FILES
 echo "Done"
 
 check_header() {
