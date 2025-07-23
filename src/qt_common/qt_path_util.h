b1b338d9ae964fc857bd4e5be5fa8329d17e1ed0
crueter <crueter@eden-emu.dev>
2025-07-22 23:23:14 -0400

    more common funcs
    Signed-off-by: crueter <crueter@eden-emu.dev>

---
+++ src/qt_common/qt_path_util.h
@@ -0,0 +1,7 @@
+#ifndef QT_PATH_UTIL_H
+#define QT_PATH_UTIL_H
+
+#include "common/common_types.h"
+namespace QtCommon::PathUtil { bool OpenShaderCache(u64 program_id); }
+
+#endif // QT_PATH_UTIL_H
--- src/yuzu/main.h
+++ src/yuzu/main.h
@@ -478,13 +478,7 @@ private:
 
     QString GetTasStateDescription() const;
     bool CreateShortcutMessagesGUI(QWidget* parent, int imsg, const QString& game_title);
-    bool MakeShortcutIcoPath(const u64 program_id, const std::string_view game_file_name,
-                             std::filesystem::path& out_icon_path);
-    bool CreateShortcutLink(const std::filesystem::path& shortcut_path, const std::string& comment,
-                            const std::filesystem::path& icon_path,
-                            const std::filesystem::path& command, const std::string& arguments,
-                            const std::string& categories, const std::string& keywords,
-                            const std::string& name);
+
     /**
      * Mimic the behavior of QMessageBox::question but link controller navigation to the dialog
      * The only difference is that it returns a boolean.
