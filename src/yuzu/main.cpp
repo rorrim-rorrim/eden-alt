bc1a0b54e83c0beb019eb62da8000abba179e25d
lizzie <lizzie@eden-emu.dev>
2025-08-13 08:32:43 +0100

    [desktop] thank you Qt Documentation, very cool Signed-off-by: crueter <crueter@eden-emu.dev>
    Signed-off-by: lizzie <lizzie@eden-emu.dev>

--- src/yuzu/main.cpp
+++ src/yuzu/main.cpp
@@ -1908,7 +1908,7 @@ void GMainWindow::HandleSigInterrupt(int sig) {
     (void)ret;
 }
 
-void GMainWindow::OnSigInterruptNotifierActivated(QSocketDescriptor _, QSocketNotifier::Type _) {
+void GMainWindow::OnSigInterruptNotifierActivated() {
     sig_interrupt_notifier->setEnabled(false);
 
     char a;
