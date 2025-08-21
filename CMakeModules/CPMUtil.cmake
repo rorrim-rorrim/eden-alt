bc5a3d50c6a8344654e77eca8d5d0896288c4162
crueter <crueter@eden-emu.dev>
2025-08-21 19:22:05 -0400

    [cmake] small optimizations + fix sdl2 download for src pack
    Signed-off-by: crueter <crueter@eden-emu.dev>

--- CMakeModules/CPMUtil.cmake
+++ CMakeModules/CPMUtil.cmake
@@ -286,6 +286,7 @@ function(AddCIPackage)
         MIN_VERSION
         DISABLED_PLATFORMS
         CMAKE_FILENAME
+        TARGET
     )
 
     cmake_parse_arguments(PKG_ARGS "" "${oneValueArgs}" "" ${ARGN})
@@ -400,19 +401,21 @@ function(AddCIPackage)
     endif()
 
     if (DEFINED ARTIFACT_DIR)
-        include(${ARTIFACT_DIR}/${ARTIFACT_CMAKE}.cmake)
+        if (NOT DEFINED PKG_ARGS_TARGET OR NOT TARGET "${PKG_ARGS_TARGET}")
+            include(${ARTIFACT_DIR}/${ARTIFACT_CMAKE}.cmake)
 
-        # Overrides find package
-        CPMAddPackage(
-            NAME ${ARTIFACT_PACKAGE}
-            SOURCE_DIR ${ARTIFACT_DIR}
-        )
+            # Overrides find package
+            CPMAddPackage(
+                NAME ${ARTIFACT_PACKAGE}
+                SOURCE_DIR ${ARTIFACT_DIR}
+            )
 
-        set_property(GLOBAL APPEND PROPERTY CPM_PACKAGE_NAMES ${ARTIFACT_NAME})
-        set_property(GLOBAL APPEND PROPERTY CPM_PACKAGE_URLS "https://github.com/${ARTIFACT_REPO}")
-        set_property(GLOBAL APPEND PROPERTY CPM_PACKAGE_SHAS ${ARTIFACT_VERSION})
+            set_property(GLOBAL APPEND PROPERTY CPM_PACKAGE_NAMES ${ARTIFACT_NAME})
+            set_property(GLOBAL APPEND PROPERTY CPM_PACKAGE_URLS "https://github.com/${ARTIFACT_REPO}") # TODO(crueter) other hosts?
+            set_property(GLOBAL APPEND PROPERTY CPM_PACKAGE_SHAS ${ARTIFACT_VERSION})
 
-        set(${ARTIFACT_PACKAGE}_ADDED TRUE PARENT_SCOPE)
+            set(${ARTIFACT_PACKAGE}_ADDED TRUE PARENT_SCOPE)
+        endif()
     else()
         find_package(${ARTIFACT_PACKAGE} ${ARTIFACT_MIN_VERSION} REQUIRED)
     endif()
