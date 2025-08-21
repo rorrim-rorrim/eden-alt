5cb2034f450eddcbf087481204c57b6719edb84e
wildcard <wildcard@eden-emu.dev>
2025-08-21 18:11:27 +0200

    [VK] change bind point from Graphics to Compute since its a compute pipeline
    Title is sufficient

--- src/video_core/renderer_vulkan/vk_compute_pass.cpp
+++ src/video_core/renderer_vulkan/vk_compute_pass.cpp
@@ -258,7 +258,7 @@ ComputePass::ComputePass(const Device& device_, DescriptorPool& descriptor_pool,
             .pDescriptorUpdateEntries = templates.data(),
             .templateType = VK_DESCRIPTOR_UPDATE_TEMPLATE_TYPE_DESCRIPTOR_SET,
             .descriptorSetLayout = *descriptor_set_layout,
-            .pipelineBindPoint = VK_PIPELINE_BIND_POINT_GRAPHICS,
+            .pipelineBindPoint = VK_PIPELINE_BIND_POINT_COMPUTE,
             .pipelineLayout = *layout,
             .set = 0,
         });
