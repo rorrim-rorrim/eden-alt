8c18344ac9cf1d07f3ca6357d8c30b868c1c27ef
SDK-Chan <sdkchan@eden-emu.dev>
2025-08-28 17:30:44 +0200

    Update src/core/hle/service/nvdrv/devices/nvhost_gpu.h

--- src/core/hle/service/nvdrv/devices/nvhost_gpu.h
+++ src/core/hle/service/nvdrv/devices/nvhost_gpu.h
@@ -184,9 +184,10 @@ private:
     NvResult SetChannelPriority(IoctlChannelSetPriority& params);
     NvResult AllocGPFIFOEx(IoctlAllocGpfifoEx& params, DeviceFD fd);
     NvResult AllocGPFIFOEx2(IoctlAllocGpfifoEx& params, DeviceFD fd);
+    
     s32_le GetObjectContextClassNumberIndex(CtxClasses class_number);
-
     NvResult AllocateObjectContext(IoctlAllocObjCtx& params);
+    
     NvResult SubmitGPFIFOImpl(IoctlSubmitGpfifo& params, Tegra::CommandList&& entries);
     
     NvResult SubmitGPFIFOBase1(IoctlSubmitGpfifo& params,
