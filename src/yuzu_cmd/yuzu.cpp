ae6646b51dca2ca66da02ce0397edef7ef5f9d89
lizzie <lizzie@eden-emu.dev>
2025-08-29 23:48:25 +0000

    [gamemode] Make available on other platforms
    Signed-off-by: lizzie <lizzie@eden-emu.dev>

--- src/yuzu_cmd/yuzu.cpp
+++ src/yuzu_cmd/yuzu.cpp
@@ -63,10 +63,7 @@ __declspec(dllexport) unsigned long NvOptimusEnablement = 0x00000001;
 __declspec(dllexport) int AmdPowerXpressRequestHighPerformance = 1;
 }
 #endif
-
-#ifdef __linux__
-#include "common/linux/gamemode.h"
-#endif
+#include "common/gamemode.h"
 
 static void PrintHelp(const char* argv0) {
     std::cout << "Usage: " << argv0
@@ -435,10 +432,7 @@ int main(int argc, char** argv) {
         // Just exit right away.
         exit(0);
     });
-
-#ifdef __linux__
-    Common::Linux::StartGamemode();
-#endif
+    Common::FeralGamemode::StartGamemode();
 
     void(system.Run());
     if (system.DebuggerEnabled()) {
@@ -450,11 +444,7 @@ int main(int argc, char** argv) {
     system.DetachDebugger();
     void(system.Pause());
     system.ShutdownMainProcess();
-
-#ifdef __linux__
-    Common::Linux::StopGamemode();
-#endif
-
+    Common::FeralGamemode::Stop();
     detached_tasks.WaitForAllTasks();
     return 0;
 }
