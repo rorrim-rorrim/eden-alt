c9dcbe66392e03f203f6d49046030c8fd67b058f
lizzie <lizzie@eden-emu.dev>
2025-08-29 23:48:25 +0000

    [gamemode] Make available on other platforms
    Signed-off-by: lizzie <lizzie@eden-emu.dev>

--- src/yuzu/main.cpp
+++ src/yuzu/main.cpp
@@ -24,9 +24,7 @@
 #include <csignal>
 #include <sys/socket.h>
 #endif
-#ifdef __linux__
-#include "common/linux/gamemode.h"
-#endif
+#include "common/gamemode.h"
 
 #include <boost/container/flat_set.hpp>
 
@@ -2274,9 +2272,7 @@ void GMainWindow::OnEmulationStopped() {
 
     discord_rpc->Update();
 
-#ifdef __linux__
-    Common::Linux::StopGamemode();
-#endif
+    Common::FeralGamemode::Stop();
 
     // The emulation is stopped, so closing the window or not does not matter anymore
     disconnect(render_window, &GRenderWindow::Closed, this, &GMainWindow::OnStopGame);
@@ -3128,10 +3124,7 @@ void GMainWindow::OnStartGame() {
     play_time_manager->Start();
 
     discord_rpc->Update();
-
-#ifdef __linux__
-    Common::Linux::StartGamemode();
-#endif
+    Common::FeralGamemode::StartGamemode();
 }
 
 void GMainWindow::OnRestartGame() {
@@ -3152,10 +3145,7 @@ void GMainWindow::OnPauseGame() {
     play_time_manager->Stop();
     UpdateMenuState();
     AllowOSSleep();
-
-#ifdef __linux__
-    Common::Linux::StopGamemode();
-#endif
+    Common::FeralGamemode::Stop();
 }
 
 void GMainWindow::OnPauseContinueGame() {
