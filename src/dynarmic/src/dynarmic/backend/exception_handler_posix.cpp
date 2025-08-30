ca098b04331f80f42b3fd78c41baf351fcf51cda
lizzie <lizzie@eden-emu.dev>
2025-08-30 19:40:25 +0000

    [nce] common ctx
    Signed-off-by: lizzie <lizzie@eden-emu.dev>

--- src/dynarmic/src/dynarmic/backend/exception_handler_posix.cpp
+++ src/dynarmic/src/dynarmic/backend/exception_handler_posix.cpp
@@ -6,6 +6,8 @@
  * SPDX-License-Identifier: 0BSD
  */
 
+#include "dynarmic/backend/exception_handler.h"
+
 #include <cstring>
 #include <functional>
 #include <memory>
@@ -119,9 +121,10 @@ void SigHandler::SigAction(int sig, siginfo_t* info, void* raw_context) {
     CTX_DECLARE(raw_context);
 #if defined(ARCHITECTURE_x86_64)
     {
-        std::shared_lock guard(sig_handler->code_block_infos_mutex);
-        if (auto const iter = sig_handler->FindCodeBlockInfo(CTX_RIP); iter != sig_handler->code_block_infos.end()) {
-            FakeCall fc = iter->second.cb(CTX_RIP);
+        std::lock_guard<std::mutex> guard(sig_handler->code_block_infos_mutex);
+        const auto iter = sig_handler->FindCodeBlockInfo(CTX_RIP);
+        if (iter != sig_handler->code_block_infos.end()) {
+            FakeCall fc = iter->cb(CTX_RIP);
             CTX_RSP -= sizeof(u64);
             *std::bit_cast<u64*>(CTX_RSP) = fc.ret_rip;
             CTX_RIP = fc.call_rip;
@@ -131,9 +134,10 @@ void SigHandler::SigAction(int sig, siginfo_t* info, void* raw_context) {
     fmt::print(stderr, "Unhandled {} at rip {:#018x}\n", sig == SIGSEGV ? "SIGSEGV" : "SIGBUS", CTX_RIP);
 #elif defined(ARCHITECTURE_arm64)
     {
-        std::shared_lock guard(sig_handler->code_block_infos_mutex);
-        if (const auto iter = sig_handler->FindCodeBlockInfo(CTX_PC); iter != sig_handler->code_block_infos.end()) {
-            FakeCall fc = iter->second.cb(CTX_PC);
+        std::lock_guard<std::mutex> guard(sig_handler->code_block_infos_mutex);
+        const auto iter = sig_handler->FindCodeBlockInfo(CTX_PC);
+        if (iter != sig_handler->code_block_infos.end()) {
+            FakeCall fc = iter->cb(CTX_PC);
             CTX_PC = fc.call_pc;
             return;
         }
