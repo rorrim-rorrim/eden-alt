2b3d2a84e665840a95421798a5b8d8d3e59fc868
lizzie <lizzie@eden-emu.dev>
2025-08-30 08:43:59 +0000

    [gamemode] extra win/lin/macos fixes
    Signed-off-by: lizzie <lizzie@eden-emu.dev>

--- src/yuzu/main.cpp
+++ src/yuzu/main.cpp
@@ -3566,7 +3566,7 @@ void GMainWindow::OnStartGame() {
     play_time_manager->Start();
 
     discord_rpc->Update();
-    Common::FeralGamemode::StartGamemode();
+    Common::FeralGamemode::Start();
 }
 
 void GMainWindow::OnRestartGame() {
