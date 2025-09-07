8153cbe10d2a0e4db06dcc9e11abab28330ef333
crueter <crueter@eden-emu.dev>
2025-09-07 19:39:35 -0400

    [cmake] fix breakpad fetching
    Signed-off-by: crueter <crueter@eden-emu.dev>

--- src/yuzu/breakpad.h
+++ src/yuzu/breakpad.h
@@ -3,8 +3,23 @@
 
 #pragma once
 
+#if defined(_WIN32)
+#include <client/windows/handler/exception_handler.h>
+#elif defined(__linux__)
+#include <client/linux/handler/exception_handler.h>
+#elif defined(__sun__)
+#include <client/solaris/handler/exception_handler.h>
+#else
+#error Minidump creation not supported on this platform
+#endif
+
 namespace Breakpad {
 
 void InstallCrashHandler();
+#if defined(__linux__) || defined(__sun__)
+[[noreturn]] bool DumpCallback(const google_breakpad::MinidumpDescriptor& descriptor,
+                  void* context,
+                  bool succeeded);
+#endif
 
 }
