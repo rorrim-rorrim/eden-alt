60785c5b466059beadb242d22b11a5b4d38872c6
lizzie <lizzie@eden-emu.dev>
2025-09-09 03:28:29 +0000

    [dynarmic] remove use of mcl reverse iterator
    Signed-off-by: lizzie <lizzie@eden-emu.dev>

--- src/dynarmic/src/dynarmic/ir/opt_passes.cpp
+++ src/dynarmic/src/dynarmic/ir/opt_passes.cpp
@@ -28,7 +28,6 @@
 #include "dynarmic/ir/type.h"
 #include "mcl/bit/swap.hpp"
 #include "mcl/bit/rotate.hpp"
-#include "mcl/iterator/reverse.hpp"
 
 namespace Dynarmic::Optimization {
 
@@ -36,50 +35,42 @@ static void A32ConstantMemoryReads(IR::Block& block, A32::UserCallbacks* cb) {
     for (auto& inst : block) {
         switch (inst.GetOpcode()) {
         case IR::Opcode::A32ReadMemory8: {
-            if (!inst.AreAllArgsImmediates()) {
-                break;
-            }
-
-            const u32 vaddr = inst.GetArg(1).GetU32();
-            if (cb->IsReadOnlyMemory(vaddr)) {
-                const u8 value_from_memory = cb->MemoryRead8(vaddr);
-                inst.ReplaceUsesWith(IR::Value{value_from_memory});
+            if (inst.AreAllArgsImmediates()) {
+                const u32 vaddr = inst.GetArg(1).GetU32();
+                if (cb->IsReadOnlyMemory(vaddr)) {
+                    const u8 value_from_memory = cb->MemoryRead8(vaddr);
+                    inst.ReplaceUsesWith(IR::Value{value_from_memory});
+                }
             }
             break;
         }
         case IR::Opcode::A32ReadMemory16: {
-            if (!inst.AreAllArgsImmediates()) {
-                break;
-            }
-
-            const u32 vaddr = inst.GetArg(1).GetU32();
-            if (cb->IsReadOnlyMemory(vaddr)) {
-                const u16 value_from_memory = cb->MemoryRead16(vaddr);
-                inst.ReplaceUsesWith(IR::Value{value_from_memory});
+            if (inst.AreAllArgsImmediates()) {
+                const u32 vaddr = inst.GetArg(1).GetU32();
+                if (cb->IsReadOnlyMemory(vaddr)) {
+                    const u16 value_from_memory = cb->MemoryRead16(vaddr);
+                    inst.ReplaceUsesWith(IR::Value{value_from_memory});
+                }
             }
             break;
         }
         case IR::Opcode::A32ReadMemory32: {
-            if (!inst.AreAllArgsImmediates()) {
-                break;
-            }
-
-            const u32 vaddr = inst.GetArg(1).GetU32();
-            if (cb->IsReadOnlyMemory(vaddr)) {
-                const u32 value_from_memory = cb->MemoryRead32(vaddr);
-                inst.ReplaceUsesWith(IR::Value{value_from_memory});
+            if (inst.AreAllArgsImmediates()) {
+                const u32 vaddr = inst.GetArg(1).GetU32();
+                if (cb->IsReadOnlyMemory(vaddr)) {
+                    const u32 value_from_memory = cb->MemoryRead32(vaddr);
+                    inst.ReplaceUsesWith(IR::Value{value_from_memory});
+                }
             }
             break;
         }
         case IR::Opcode::A32ReadMemory64: {
-            if (!inst.AreAllArgsImmediates()) {
-                break;
-            }
-
-            const u32 vaddr = inst.GetArg(1).GetU32();
-            if (cb->IsReadOnlyMemory(vaddr)) {
-                const u64 value_from_memory = cb->MemoryRead64(vaddr);
-                inst.ReplaceUsesWith(IR::Value{value_from_memory});
+            if (inst.AreAllArgsImmediates()) {
+                const u32 vaddr = inst.GetArg(1).GetU32();
+                if (cb->IsReadOnlyMemory(vaddr)) {
+                    const u64 value_from_memory = cb->MemoryRead64(vaddr);
+                    inst.ReplaceUsesWith(IR::Value{value_from_memory});
+                }
             }
             break;
         }
@@ -1205,11 +1196,9 @@ static void ConstantPropagation(IR::Block& block) {
 static void DeadCodeElimination(IR::Block& block) {
     // We iterate over the instructions in reverse order.
     // This is because removing an instruction reduces the number of uses for earlier instructions.
-    for (auto& inst : mcl::iterator::reverse(block)) {
-        if (!inst.HasUses() && !MayHaveSideEffects(inst.GetOpcode())) {
-            inst.Invalidate();
-        }
-    }
+    for (auto it = block.rbegin(); it != block.rend(); ++it)
+        if (!it->HasUses() && !MayHaveSideEffects(it->GetOpcode()))
+            it->Invalidate();
 }
 
 static void IdentityRemovalPass(IR::Block& block) {
