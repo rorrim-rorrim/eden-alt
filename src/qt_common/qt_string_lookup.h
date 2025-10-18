107372df595a9e2d06d0040eec8795e758c936c7
crueter <crueter@eden-emu.dev>
2025-10-18 13:54:36 -0400

    [qt] fix translations
    Linguist strongly dislikes lookup tables of this sort due to the fact
    that it looks for tr(), qsTr(), etc. when determining what strings need
    translations. To fix this, we can create a "fake" tr function that gets
    Linguist to recognize the strings while keeping them constexpr; then,
    the Lookup function can translate them at runtime.
    
    Signed-off-by: crueter <crueter@eden-emu.dev>

--- src/qt_common/qt_string_lookup.h
+++ src/qt_common/qt_string_lookup.h
@@ -4,14 +4,19 @@
 #pragma once
 
 #include <QString>
-#include <frozen/string.h>
-#include <frozen/unordered_map.h>
-#include <qobjectdefs.h>
-#include <qtmetamacros.h>
+#include "frozen/map.h"
+#include "frozen/string.h"
+#include <QObject>
 
 namespace QtCommon::StringLookup {
 
 Q_NAMESPACE
+
+// fake tr to make linguist happy
+constexpr const frozen::string tr(const char *data)
+{
+    return frozen::string{data};
+}
 
 // TODO(crueter): QML interface
 enum StringKey {
@@ -20,19 +25,53 @@ enum StringKey {
     UserNandTooltip,
     SysNandTooltip,
     ModsTooltip,
+
+    // Key install results
+    KeyInstallSuccess,
+    KeyInstallInvalidDir,
+    KeyInstallErrorFailedCopy,
+    KeyInstallErrorWrongFilename,
+    KeyInstallErrorFailedInit,
+
+    // Firmware install results
+    FwInstallSuccess,
+    FwInstallNoOp,
+    FwInstallNoNCAs,
+    FwInstallFailedDelete,
+    FwInstallFailedCopy,
+    FwInstallFailedCorrupted,
 };
 
-static constexpr const frozen::unordered_map<StringKey, frozen::string, 5> strings = {
-    {SavesTooltip, "Contains game save data. DO NOT REMOVE UNLESS YOU KNOW WHAT YOU'RE DOING!"},
-    {ShadersTooltip, "Contains Vulkan and OpenGL pipeline caches. Generally safe to remove."},
-    {UserNandTooltip, "Contains updates and DLC for games."},
-    {SysNandTooltip, "Contains firmware and applet data."},
-    {ModsTooltip, "Contains game mods, patches, and cheats."},
+static const frozen::map<StringKey, frozen::string, 20> strings = {
+    {SavesTooltip, tr("Contains game save data. DO NOT REMOVE UNLESS YOU KNOW WHAT YOU'RE DOING!")},
+    {ShadersTooltip, tr("Contains Vulkan and OpenGL pipeline caches. Generally safe to remove.")},
+    {UserNandTooltip, tr("Contains updates and DLC for games.")},
+    {SysNandTooltip, tr("Contains firmware and applet data.")},
+    {ModsTooltip, tr("Contains game mods, patches, and cheats.")},
+
+    // Key install results
+    {KeyInstallSuccess, tr("Decryption Keys were successfully installed")},
+    {KeyInstallInvalidDir, tr("Unable to read key directory, aborting")},
+    {KeyInstallErrorFailedCopy, tr("One or more keys failed to copy.")},
+    {KeyInstallErrorWrongFilename, tr("Verify your keys file has a .keys extension and try again.")},
+    {KeyInstallErrorFailedInit,
+     tr("Decryption Keys failed to initialize. Check that your dumping tools are up to date and "
+        "re-dump keys.")},
+
+    // fw install
+    {FwInstallSuccess, tr("Successfully installed firmware version %1")},
+    {FwInstallNoOp, tr("")},
+    {FwInstallNoNCAs, tr("Unable to locate potential firmware NCA files")},
+    {FwInstallFailedDelete, tr("Failed to delete one or more firmware files.")},
+    {FwInstallFailedCopy, tr("One or more firmware files failed to copy into NAND.")},
+    {FwInstallFailedCorrupted,
+     tr("Firmware installation cancelled, firmware may be in a bad state or corrupted. Restart "
+        "Eden or re-install firmware.")},
 };
 
 static inline const QString Lookup(StringKey key)
 {
-    return QString::fromStdString(strings.at(key).data());
+    return QObject::tr(strings.at(key).data());
 }
 
-}
+} // namespace QtCommon::StringLookup
