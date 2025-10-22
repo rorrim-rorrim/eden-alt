83744266aa636231eee28a1bf7868157324282f9
crueter <crueter@eden-emu.dev>
2025-10-22 16:23:58 -0400

    WIP: [qt] Ryujinx save data link
    This adds an action to the Game List context menu that lets users link
    save data from Eden to Ryujinx, or vice versa. I'll document this more
    later, but basically the gist of it is:
    - read title_id -> save_id pairs from imkvdb.arc
    - find a match in ryujinx
    - if it exists, symlink it
    
    This needs extensive testing on Windows. I have no idea if `mklink /J`
    will work how I want it to. But it is confirmed working on Linux (minus
    one of my drives being exfat which is... mildly annoying)
    
    Signed-off-by: crueter <crueter@eden-emu.dev>

---
+++ src/yuzu/ryujinx_dialog.cpp
@@ -0,0 +1,65 @@
+#include "qt_common/abstract/qt_frontend_util.h"
+#include "ryujinx_dialog.h"
+#include "ui_ryujinx_dialog.h"
+#include <filesystem>
+#include <system_error>
+#include <fmt/format.h>
+
+namespace fs = std::filesystem;
+
+RyujinxDialog::RyujinxDialog(std::filesystem::path eden_path,
+                             std::filesystem::path ryu_path,
+                             QWidget *parent)
+    : QDialog(parent)
+    , ui(new Ui::RyujinxDialog)
+    , m_eden(eden_path)
+    , m_ryu(ryu_path)
+{
+    ui->setupUi(this);
+
+    connect(ui->eden, &QPushButton::clicked, this, &RyujinxDialog::fromEden);
+    connect(ui->ryujinx, &QPushButton::clicked, this, &RyujinxDialog::fromRyujinx);
+}
+
+RyujinxDialog::~RyujinxDialog()
+{
+    delete ui;
+}
+
+void RyujinxDialog::fromEden()
+{
+    accept();
+    link(m_eden, m_ryu);
+}
+
+void RyujinxDialog::fromRyujinx()
+{
+    accept();
+    link(m_ryu, m_eden);
+}
+
+void RyujinxDialog::link(std::filesystem::path &from, std::filesystem::path &to)
+{
+    std::error_code ec;
+
+    // "ignore" errors--if the dir fails to be deleted, error handling later will handle it
+    fs::remove_all(to, ec);
+
+#ifdef _WIN32
+    const std::string command = fmt::format("mklink /J {} {}", to.string(), from.string());
+    system(command.c_str());
+#else
+    try {
+        fs::create_directory_symlink(from, to);
+    } catch (std::exception &e) {
+        QtCommon::Frontend::Critical(tr("Failed to link save data"),
+                                     tr("Could not link directory:\n\t%1\nTo:\n\t%2\n\nError: %3")
+                                         .arg(QString::fromStdString(from.string()),
+                                              QString::fromStdString(to.string()),
+                                              QString::fromStdString(e.what())));
+        return;
+    }
+#endif
+
+    QtCommon::Frontend::Information(tr("Linked Save Data"), tr("Save data has been linked."));
+}
---
+++ src/yuzu/ryujinx_dialog.h
@@ -0,0 +1,34 @@
+#ifndef RYUJINX_DIALOG_H
+#define RYUJINX_DIALOG_H
+
+#include <QDialog>
+#include <filesystem>
+
+namespace Ui {
+class RyujinxDialog;
+}
+
+class RyujinxDialog : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit RyujinxDialog(std::filesystem::path eden_path, std::filesystem::path ryu_path, QWidget *parent = nullptr);
+    ~RyujinxDialog();
+
+private slots:
+    void fromEden();
+    void fromRyujinx();
+
+private:
+    Ui::RyujinxDialog *ui;
+    std::filesystem::path m_eden;
+    std::filesystem::path m_ryu;
+
+    /// @brief Link two directories
+    /// @param from The symlink target
+    /// @param to   The symlink name (will be deleted)
+    void link(std::filesystem::path &from, std::filesystem::path &to);
+};
+
+#endif // RYUJINX_DIALOG_H
