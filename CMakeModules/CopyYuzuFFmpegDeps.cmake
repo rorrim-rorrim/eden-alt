bbe7702983f1b1abf6f1d92db8ec8e0c14a2fd36
crueter <crueter@eden-emu.dev>
2025-11-02 21:01:49 -0500

    [cmake] better FFmpeg debugging; remove obsolete CopyYuzuSDLDeps
    Ideally, we remove CopyYuzuFFmpegDeps and just use a static FFmpeg
    instead, unfortunately cubeb conflicts currently; something with ksuser?
    need to investigate further now that I actually understand Windows
    syslib fuckery
    
    Signed-off-by: crueter <crueter@eden-emu.dev>

--- CMakeModules/CopyYuzuFFmpegDeps.cmake
+++ CMakeModules/CopyYuzuFFmpegDeps.cmake
@@ -1,10 +1,17 @@
+# SPDX-FileCopyrightText: 2025 Eden Emulator Project
+# SPDX-License-Identifier: GPL-3.0-or-later
+
 # SPDX-FileCopyrightText: 2020 yuzu Emulator Project
 # SPDX-License-Identifier: GPL-2.0-or-later
 
+# TODO(crueter): Remove this entirely; notably, cubeb and ffmpeg conflict due to some weird ksuser stuff
 function(copy_yuzu_FFmpeg_deps target_dir)
     include(WindowsCopyFiles)
     set(DLL_DEST "$<TARGET_FILE_DIR:${target_dir}>/")
     file(READ "${FFmpeg_PATH}/requirements.txt" FFmpeg_REQUIRED_DLLS)
     string(STRIP "${FFmpeg_REQUIRED_DLLS}" FFmpeg_REQUIRED_DLLS)
+
+    message(STATUS "[CopyYuzuFFmpegDeps] Copying FFmpeg deps from ${FFmpeg_LIBRARY_DIR} to ${DLL_DEST}")
+    message(STATUS "[CopyYuzuFFmpegDeps] FFmpeg DLLs: ${FFmpeg_REQUIRED_DLLS}")
     windows_copy_files(${target_dir} ${FFmpeg_LIBRARY_DIR} ${DLL_DEST} ${FFmpeg_REQUIRED_DLLS})
 endfunction(copy_yuzu_FFmpeg_deps)
