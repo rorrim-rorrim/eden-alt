6ed669f685221d3e997d600e02ab477ef8ecde6f
lizzie <lizzie@eden-emu.dev>
2025-11-03 01:30:56 +0000

    first emitted block
    Signed-off-by: lizzie <lizzie@eden-emu.dev>

--- src/dynarmic/src/dynarmic/backend/ppc64/code_block.h
+++ src/dynarmic/src/dynarmic/backend/ppc64/code_block.h
@@ -15,22 +15,23 @@ namespace Dynarmic::Backend::PPC64 {
 
 class CodeBlock {
 public:
-    explicit CodeBlock(std::size_t size) noexcept : memsize(size) {
-        mem = (u8*)mmap(nullptr, size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
-        ASSERT(mem != nullptr);
+    explicit CodeBlock(size_t size_) noexcept : size{size_} {
+        block = (u8*)mmap(nullptr, size, PROT_WRITE | PROT_READ | PROT_EXEC, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
+        //printf("block = %p, size= %zx\n", block, size);
+        ASSERT(block != nullptr);
     }
     ~CodeBlock() noexcept {
-        if (mem != nullptr)
-            munmap(mem, memsize);
+        if (block != nullptr)
+            munmap(block, size);
     }
     template<typename T>
     T ptr() const noexcept {
         static_assert(std::is_pointer_v<T> || std::is_same_v<T, uintptr_t> || std::is_same_v<T, intptr_t>);
-        return reinterpret_cast<T>(mem);
+        return reinterpret_cast<T>(block);
     }
 protected:
-    u8* mem = nullptr;
-    size_t memsize = 0;
+    void* block = nullptr;
+    size_t size = 0;
 };
 
 }  // namespace Dynarmic::Backend::RV64
