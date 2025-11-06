93acae78a6461d261120acb01040125a219fbaa6
JPikachu <jpikachu.eden@gmail.com>
2025-11-06 17:59:19 +0000

    Revert "[vk] Fix AMD windows"
    This reverts commit 7ce2746fca9123bc11ed067f1ab5ceabd79a8590.

--- src/video_core/renderer_vulkan/vk_rasterizer.cpp
+++ src/video_core/renderer_vulkan/vk_rasterizer.cpp
@@ -952,10 +952,7 @@ void RasterizerVulkan::UpdateDynamicStates() {
     UpdateDepthBounds(regs);
     UpdateStencilFaces(regs);
     UpdateLineWidth(regs);
-
-    const u8 dynamic_state = Settings::values.dyna_state.GetValue();
-
-    if (device.IsExtExtendedDynamicStateSupported() && dynamic_state > 0) {
+    if (device.IsExtExtendedDynamicStateSupported()) {
         UpdateCullMode(regs);
         UpdateDepthCompareOp(regs);
         UpdateFrontFace(regs);
@@ -966,13 +963,13 @@ void RasterizerVulkan::UpdateDynamicStates() {
             UpdateDepthTestEnable(regs);
             UpdateDepthWriteEnable(regs);
             UpdateStencilTestEnable(regs);
-            if (device.IsExtExtendedDynamicState2Supported() && dynamic_state > 1) {
+            if (device.IsExtExtendedDynamicState2Supported()) {
                 UpdatePrimitiveRestartEnable(regs);
                 UpdateRasterizerDiscardEnable(regs);
                 UpdateDepthBiasEnable(regs);
             }
 
-            if (device.IsExtExtendedDynamicState3EnablesSupported() && dynamic_state > 2) {
+            if (device.IsExtExtendedDynamicState3Supported()) {
                 using namespace Tegra::Engines;
 
                 if (device.GetDriverID() == VkDriverIdKHR::VK_DRIVER_ID_AMD_OPEN_SOURCE ||
@@ -999,10 +996,10 @@ void RasterizerVulkan::UpdateDynamicStates() {
                 UpdateDepthClampEnable(regs);
             }
         }
-        if (device.IsExtExtendedDynamicState2ExtrasSupported() && dynamic_state > 1) {
+        if (device.IsExtExtendedDynamicState2ExtrasSupported()) {
             UpdateLogicOp(regs);
         }
-        if (device.IsExtExtendedDynamicState3BlendingSupported() && dynamic_state > 2) {
+        if (device.IsExtExtendedDynamicState3Supported()) {
             UpdateBlending(regs);
         }
     }
