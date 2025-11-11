7f1c79a52178ac20caa741eb32ab0327e7a96e21
lizzie <lizzie@eden-emu.dev>
2025-11-11 01:52:19 +0000

    [common] fix formatting of swapped u32_le/u64_le for BE targets
    Signed-off-by: lizzie <lizzie@eden-emu.dev>

--- src/common/logging/formatter.h
+++ src/common/logging/formatter.h
@@ -4,8 +4,8 @@
 #pragma once
 
 #include <type_traits>
-
 #include <fmt/ranges.h>
+#include "common/swap.h"
 
 // adapted from https://github.com/fmtlib/fmt/issues/2704
 // a generic formatter for enum classes
@@ -20,3 +20,14 @@ struct fmt::formatter<T, std::enable_if_t<std::is_enum_v<T>, char>>
     }
 };
 #endif
+
+template <typename T, typename U>
+struct fmt::formatter<SwapStructT<T, U>> {
+    constexpr auto parse(format_parse_context& ctx) {
+        return ctx.begin();
+    }
+    template <typename FormatContext>
+    auto format(const SwapStructT<T, U>& reg, FormatContext& ctx) const {
+        return fmt::format_to(ctx.out(), "{}", T(reg));
+    }
+};
