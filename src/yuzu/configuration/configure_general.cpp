2a8772ce3e8c2e21dae67b21193458a444cd6e9e
lizzie <lizzie@eden-emu.dev>
2025-11-24 08:54:35 +0000

    remove last remnants of layout

--- src/yuzu/configuration/configure_general.cpp
+++ src/yuzu/configuration/configure_general.cpp
@@ -40,8 +40,6 @@ void ConfigureGeneral::SetConfiguration() {}
 
 void ConfigureGeneral::Setup(const ConfigurationShared::Builder& builder) {
     QLayout& general_layout = *ui->general_widget->layout();
-    QLayout& linux_layout = *ui->linux_widget->layout();
-
     std::map<u32, QWidget*> general_hold{};
     std::map<u32, QWidget*> linux_hold{};
 
@@ -54,12 +52,6 @@ void ConfigureGeneral::Setup(const ConfigurationShared::Builder& builder) {
     };
 
     push(UISettings::values.linkage.by_category[Settings::Category::UiGeneral]);
-
-    // Only show Linux group on Unix
-#ifndef __unix__
-    ui->LinuxGroupBox->setVisible(false);
-#endif
-
     for (const auto setting : settings) {
         auto* widget = builder.BuildWidget(setting, apply_funcs);
 
@@ -82,9 +74,6 @@ void ConfigureGeneral::Setup(const ConfigurationShared::Builder& builder) {
 
     for (const auto& [id, widget] : general_hold) {
         general_layout.addWidget(widget);
-    }
-    for (const auto& [id, widget] : linux_hold) {
-        linux_layout.addWidget(widget);
     }
 }
 
