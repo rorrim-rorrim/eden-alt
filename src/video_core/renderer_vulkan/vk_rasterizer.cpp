00e88fab2b4ac5c3e5353df81a53b7e56c32397e
MaranBr <maranbr@outlook.com>
2025-11-24 11:02:51 -0400

    ?ploo

--- src/video_core/renderer_vulkan/vk_rasterizer.cpp
+++ src/video_core/renderer_vulkan/vk_rasterizer.cpp
@@ -74,10 +74,10 @@ VkViewport GetViewportState(const Device& device, const Maxwell& regs, size_t in
     float width = conv(src.scale_x * 2.0f);
     float y = conv(src.translate_y - src.scale_y);
     float height = conv(src.scale_y * 2.0f);
-    if (regs.window_origin.mode == Maxwell::WindowOrigin::Mode::UpperLeft) {
-        y = conv(static_cast<float>(regs.surface_clip.height)) - y - height;
-    }
-    if (!device.IsNvViewportSwizzleSupported() && src.swizzle.y == Maxwell::ViewportSwizzle::NegativeY) {
+    if (regs.window_origin.mode != Maxwell::WindowOrigin::Mode::UpperLeft) {
+        y += conv(static_cast<float>(regs.surface_clip.height));
+        height = -height;
+    } else if (!device.IsNvViewportSwizzleSupported() && src.swizzle.y == Maxwell::ViewportSwizzle::NegativeY) {
         y += height;
         height = -height;
     }
