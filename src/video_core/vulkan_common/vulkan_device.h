e3b10eba104f0c4a5a941e46328bbf97364923bc
CamilleLaVey <camillelavey99@gmail.com>
2025-11-26 00:01:42 -0400

    [maxwell, vk] VK_EXT_Sample_Locations

--- src/video_core/vulkan_common/vulkan_device.h
+++ src/video_core/vulkan_common/vulkan_device.h
@@ -63,6 +63,7 @@ VK_DEFINE_HANDLE(VmaAllocator)
     FEATURE(EXT, ProvokingVertex, PROVOKING_VERTEX, provoking_vertex)                              \
     FEATURE(EXT, Robustness2, ROBUSTNESS_2, robustness2)                                           \
     FEATURE(EXT, TransformFeedback, TRANSFORM_FEEDBACK, transform_feedback)                        \
+    FEATURE(EXT, SampleLocations, SAMPLE_LOCATIONS, sample_locations)                              \
     FEATURE(EXT, VertexInputDynamicState, VERTEX_INPUT_DYNAMIC_STATE, vertex_input_dynamic_state)  \
     FEATURE(EXT, SwapchainMaintenance1, SWAPCHAIN_MAINTENANCE_1, swapchain_maintenance1)           \
     FEATURE(KHR, Maintenance5, MAINTENANCE_5, maintenance5)                                        \
@@ -342,6 +343,11 @@ public:
         return properties.float_controls;
     }
 
+    /// Returns sample location properties (VK_EXT_sample_locations).
+    const VkPhysicalDeviceSampleLocationsPropertiesEXT& SampleLocationProperties() const {
+        return properties.sample_locations;
+    }
+
     /// Returns true if ASTC is natively supported.
     bool IsOptimalAstcSupported() const {
         return features.features.textureCompressionASTC_LDR;
@@ -548,6 +554,17 @@ public:
     /// Returns true if the device supports VK_EXT_transform_feedback.
     bool IsExtTransformFeedbackSupported() const {
         return extensions.transform_feedback;
+    }
+
+    /// Returns true if the device supports VK_EXT_sample_locations.
+    bool IsExtSampleLocationsSupported() const {
+        return extensions.sample_locations;
+    }
+
+    /// Returns true if the device supports custom sample locations for the given sample count.
+    bool SupportsSampleLocationsFor(VkSampleCountFlagBits samples) const {
+        return extensions.sample_locations &&
+               (properties.sample_locations.sampleLocationSampleCounts & samples) != 0;
     }
 
     /// Returns true if the device supports VK_EXT_transform_feedback properly.
@@ -1017,6 +1034,7 @@ private:
         VkPhysicalDevicePushDescriptorPropertiesKHR push_descriptor{};
         VkPhysicalDeviceSubgroupSizeControlProperties subgroup_size_control{};
         VkPhysicalDeviceTransformFeedbackPropertiesEXT transform_feedback{};
+        VkPhysicalDeviceSampleLocationsPropertiesEXT sample_locations{};
         VkPhysicalDeviceMaintenance5PropertiesKHR maintenance5{};
         VkPhysicalDeviceMultiDrawPropertiesEXT multi_draw{};
 
