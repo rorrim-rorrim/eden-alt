816eb96f4814ebee5397591cabfe44976ca54b0e
lizzie <lizzie@eden-emu.dev>
2025-11-30 20:36:53 +0000

    fix macos shenanigans

--- src/video_core/vulkan_common/vk_enum_string_helper.h
+++ src/video_core/vulkan_common/vk_enum_string_helper.h
@@ -5,4 +5,7 @@
 
 #include "video_core/vulkan_common/vulkan.h"
 
+#if defined(__APPLE__) && !defined(VK_STRUCTURE_TYPE_OH_SURFACE_CREATE_INFO_OHOS)
+#   define VK_STRUCTURE_TYPE_OH_SURFACE_CREATE_INFO_OHOS VK_STRUCTURE_TYPE_SURFACE_CREATE_INFO_OHOS
+#endif
 #include <vulkan/vk_enum_string_helper.h>
