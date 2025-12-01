6e5b5de53692468aa95f7774558623483e26f6da
CamilleLaVey <camillelavey99@gmail.com>
2025-12-01 12:59:01 -0400

    Hotfix

--- src/video_core/renderer_vulkan/vk_rasterizer.cpp
+++ src/video_core/renderer_vulkan/vk_rasterizer.cpp
@@ -1671,7 +1671,7 @@ void RasterizerVulkan::UpdateStencilOp(Tegra::Engines::Maxwell3D::Regs& regs) {
     }
 }
 
-void RasterizerVulkan::UpdateSampleLocations(Maxwell::Regs& regs) {
+void RasterizerVulkan::UpdateSampleLocations(Maxwell& regs) {
     if (!device.IsExtSampleLocationsSupported()) {
         return;
     }
