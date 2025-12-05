ddf441d30e82a4011349445d6eb7a9af6f21143a
CamilleLaVey <camillelavey99@gmail.com>
2025-12-05 09:18:41 -0400

    [revert] SL Table order

--- src/video_core/vulkan_common/vulkan_device.h
+++ src/video_core/vulkan_common/vulkan_device.h
@@ -979,12 +979,7 @@ public:
         return nvidia_arch;
     }
 
-    static constexpr size_t SampleLocationTableSize() {
-        return sample_location_table_size;
-    }
-
 private:
-
     static constexpr size_t sample_location_table_size = 7;
 
     /// Checks if the physical device is suitable and configures the object state
