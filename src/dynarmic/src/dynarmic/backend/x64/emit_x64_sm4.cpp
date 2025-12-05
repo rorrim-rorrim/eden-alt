0321919e10044eadee574ccca7f8f1e4ec1ebfa8
lizzie <lizzie@eden-emu.dev>
2025-12-05 16:03:39 +0000

    [dynarmic] x86 remove BlockOfCode& ref on regalloc

--- src/dynarmic/src/dynarmic/backend/x64/emit_x64_sm4.cpp
+++ src/dynarmic/src/dynarmic/backend/x64/emit_x64_sm4.cpp
@@ -13,7 +13,7 @@ namespace Dynarmic::Backend::X64 {
 void EmitX64::EmitSM4AccessSubstitutionBox(EmitContext& ctx, IR::Inst* inst) {
     auto args = ctx.reg_alloc.GetArgumentInfo(inst);
 
-    ctx.reg_alloc.HostCall(inst, args[0]);
+    ctx.reg_alloc.HostCall(code, inst, args[0]);
     code.CallFunction(&Common::Crypto::SM4::AccessSubstitutionBox);
     code.movzx(code.ABI_RETURN.cvt32(), code.ABI_RETURN.cvt8());
 }
