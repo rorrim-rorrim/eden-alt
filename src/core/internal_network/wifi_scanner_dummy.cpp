9afeca85ecb612ee98c53850d8ad8a872c946c4d
lizzie <lizzie@eden-emu.dev>
2025-12-10 03:44:44 +0000

    etc fixes

--- src/core/internal_network/wifi_scanner_dummy.cpp
+++ src/core/internal_network/wifi_scanner_dummy.cpp
@@ -1,6 +1,9 @@
 // SPDX-FileCopyrightText: Copyright 2025 Eden Emulator Project
 // SPDX-License-Identifier: GPL-3.0-or-later
 
+#include <chrono>
+#include "core/internal_network/wifi_scanner.h"
+
 namespace Network {
 std::vector<Network::ScanData> ScanWifiNetworks(std::chrono::milliseconds deadline) {
     return {}; // disabled, pretend no results
