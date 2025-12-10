e2f175f82d8e4ea25528d3f670d442cee8b9b27f
MaranBr <maranbr@outlook.com>
2025-12-10 08:15:25 -0400

    Fix GPU mode descriptions

--- src/qt_common/config/shared_translation.cpp
+++ src/qt_common/config/shared_translation.cpp
@@ -277,7 +277,7 @@ std::unique_ptr<TranslationMap> InitializeTranslations(QObject* parent)
     INSERT(Settings,
            gpu_accuracy,
            tr("GPU Accuracy:"),
-           tr("Controls the GPU emulation accuracy.\nMost games render fine with Performance or Balanced modes, but Accurate is still "
+           tr("Controls the GPU emulation accuracy.\nMost games render fine with Fast or Balanced modes, but Accurate is still "
               "required for some.\nParticles tend to only render correctly with Accurate mode."));
     INSERT(Settings,
            dma_accuracy,
@@ -506,7 +506,7 @@ std::unique_ptr<ComboboxTranslationMap> ComboboxEnumeration(QObject* parent)
          }});
     translations->insert({Settings::EnumMetadata<Settings::GpuAccuracy>::Index(),
                           {
-                              PAIR(GpuAccuracy, Low, tr("Performance")),
+                              PAIR(GpuAccuracy, Low, tr("Fast")),
                               PAIR(GpuAccuracy, Medium, tr("Balanced")),
                               PAIR(GpuAccuracy, High, tr("Accurate")),
                           }});
