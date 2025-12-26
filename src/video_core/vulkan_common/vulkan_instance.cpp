6739f95c210db7e7ec40facab91125dc40c9dd33
lizzie <lizzie@eden-emu.dev>
2025-12-26 02:46:15 +0000

    remove weird conditional

--- src/video_core/vulkan_common/vulkan_instance.cpp
+++ src/video_core/vulkan_common/vulkan_instance.cpp
@@ -79,9 +79,6 @@ namespace {
 #endif
         if (enable_validation && AreExtensionsSupported(dld, *properties, std::array{VK_EXT_DEBUG_UTILS_EXTENSION_NAME}))
             extensions.push_back(VK_EXT_DEBUG_UTILS_EXTENSION_NAME);
-        // VK_EXT_surface_maintenance1 is required for VK_EXT_swapchain_maintenance1
-        if (window_type != Core::Frontend::WindowSystemType::Headless && AreExtensionsSupported(dld, *properties, std::array{VK_EXT_SURFACE_MAINTENANCE_1_EXTENSION_NAME}))
-            extensions.push_back(VK_EXT_SURFACE_MAINTENANCE_1_EXTENSION_NAME);
     }
     return extensions;
 }
