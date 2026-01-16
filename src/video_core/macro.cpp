4881bf28bba0ca6744c99faff735b4eabbea39e6
lizzie <lizzie@eden-emu.dev>
2026-01-16 03:27:08 +0000

    fuck you

--- src/video_core/macro.cpp
+++ src/video_core/macro.cpp
@@ -21,14 +21,6 @@
 #pragma clang diagnostic ignored "-Wshadow"
 #endif
 #include <xbyak/xbyak.h>
-
-#ifdef __GNUC__
-#pragma GCC diagnostic pop
-#endif
-#ifdef __clang__
-#pragma clang diagnostic pop
-#endif
-
 #endif
 
 #include "common/assert.h"
