baea34dcd8051be4ea9ede4705a36ceedb5e7c99
lizzie <lizzie@eden-emu.dev>
2026-01-25 01:00:47 +0000

    fuckyou2

--- src/video_core/renderer_vulkan/fixed_pipeline_state.h
+++ src/video_core/renderer_vulkan/fixed_pipeline_state.h
@@ -227,7 +227,7 @@ struct FixedPipelineState {
     std::array<u16, Maxwell::NumViewports> viewport_swizzles;
 
     // TODO: this has to be trivially constructuible and both are mutually exclusive
-    std::array<u64, 3> attribute_types; // Used with VK_EXT_vertex_input_dynamic_state
+    std::array<u32, 4> attribute_types; // Used with VK_EXT_vertex_input_dynamic_state
     u64 enabled_divisors;
 
     DynamicState dynamic_state;
