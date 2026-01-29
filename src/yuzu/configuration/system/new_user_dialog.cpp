051be61b58417d8c471037a4cbe892072d2ed003
crueter <crueter@eden-emu.dev>
2026-01-28 22:24:18 -0500

    Fix license headers

--- src/yuzu/configuration/system/new_user_dialog.cpp
+++ src/yuzu/configuration/system/new_user_dialog.cpp
@@ -1,3 +1,6 @@
+// SPDX-FileCopyrightText: Copyright 2026 Eden Emulator Project
+// SPDX-License-Identifier: GPL-3.0-or-later
+
 #include <algorithm>
 #include "common/common_types.h"
 #include "common/fs/path_util.h"
