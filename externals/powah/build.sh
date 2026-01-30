6839ff0385909b3c96c78be870f456f766855b64
lizzie <lizzie@eden-emu.dev>
2026-01-30 19:22:04 +0000

    update loicense

--- externals/powah/build.sh
+++ externals/powah/build.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
+
+# SPDX-FileCopyrightText: Copyright 2026 Eden Emulator Project
+# SPDX-License-Identifier: GPL-3.0-or-later
 $CC data2code.c -o data2code && ./data2code data.txt >powah_gen_base.hpp
 $CC --target=powerpc64le test.S -c -o test && objdump -SC test
