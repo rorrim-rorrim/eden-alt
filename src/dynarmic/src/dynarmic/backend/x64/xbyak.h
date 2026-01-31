7e74fca77efeaee26d3821f33d17106e498727b5
lizzie <lizzie@eden-emu.dev>
2026-01-31 10:22:02 +0000

    [dynarmic] make Xbyak::CodeGen use Ankerl for backing unordered maps
    Signed-off-by: lizzie <lizzie@eden-emu.dev>

---
+++ src/dynarmic/src/dynarmic/backend/x64/xbyak.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#define XBYAK_STD_UNORDERED_SET ankerl::unordered_dense::set
+#define XBYAK_STD_UNORDERED_MAP ankerl::unordered_dense::map
+#define XBYAK_STD_UNORDERED_MULTIMAP std::unordered_multimap
+
+#include <ankerl/unordered_dense.h>
+#include <unordered_map>
+
+#include <xbyak/xbyak.h>
+#include <xbyak/xbyak_util.h>
