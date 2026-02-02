31a111677276ed6bbcf298aaaa9d3ab4320eaf41
lizzie <lizzie@eden-emu.dev>
2026-02-02 23:12:17 +0000

    leave pending param package stuff

--- src/common/param_package.cpp
+++ src/common/param_package.cpp
@@ -75,7 +75,9 @@ std::string ParamPackage::Serialize() const {
 std::string ParamPackage::Get(const std::string& key, const std::string& default_value) const {
     auto pair = data.find(key);
     if (pair == data.end()) {
+#ifndef __OPENORBIS__
         LOG_TRACE(Common, "key '{}' not found", key);
+#endif
         return default_value;
     }
 
@@ -85,7 +87,9 @@ std::string ParamPackage::Get(const std::string& key, const std::string& default
 int ParamPackage::Get(const std::string& key, int default_value) const {
     auto pair = data.find(key);
     if (pair == data.end()) {
+#ifndef __OPENORBIS__
         LOG_TRACE(Common, "key '{}' not found", key);
+#endif
         return default_value;
     }
 
@@ -100,7 +104,9 @@ int ParamPackage::Get(const std::string& key, int default_value) const {
 float ParamPackage::Get(const std::string& key, float default_value) const {
     auto pair = data.find(key);
     if (pair == data.end()) {
+#ifndef __OPENORBIS__
         LOG_TRACE(Common, "key {} not found", key);
+#endif
         return default_value;
     }
 
