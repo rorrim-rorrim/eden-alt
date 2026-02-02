9af415d28816d37041265074ccdb5e830d5a8ea6
lizzie <lizzie@eden-emu.dev>
2026-02-02 23:11:58 +0000

    old xbyak behaviour for now

--- src/dynarmic/src/dynarmic/backend/x64/xbyak.h
+++ src/dynarmic/src/dynarmic/backend/x64/xbyak.h
@@ -1,15 +1,14 @@
 #pragma once
 
+#include <unordered_map>
+#include <unordered_set>
+
 #ifdef __OPENORBIS__
 #define XBYAK_NO_EXCEPTION 1
 #endif
-
-#define XBYAK_STD_UNORDERED_SET ankerl::unordered_dense::set
-#define XBYAK_STD_UNORDERED_MAP ankerl::unordered_dense::map
+#define XBYAK_STD_UNORDERED_SET std::unordered_set
+#define XBYAK_STD_UNORDERED_MAP std::unordered_map
 #define XBYAK_STD_UNORDERED_MULTIMAP std::unordered_multimap
-
-#include <ankerl/unordered_dense.h>
-#include <unordered_map>
 
 #include <xbyak/xbyak.h>
 #include <xbyak/xbyak_util.h>
