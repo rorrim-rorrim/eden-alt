1cf844314864393a00cfc1c93a286270c02702f2
lizzie <lizzie@eden-emu.dev>
2026-02-02 08:08:43 +0000

    ok boost multimap

--- src/dynarmic/src/dynarmic/backend/x64/xbyak.h
+++ src/dynarmic/src/dynarmic/backend/x64/xbyak.h
@@ -5,10 +5,10 @@
 
 #define XBYAK_STD_UNORDERED_SET ankerl::unordered_dense::set
 #define XBYAK_STD_UNORDERED_MAP ankerl::unordered_dense::map
-#define XBYAK_STD_UNORDERED_MULTIMAP std::unordered_multimap
+#define XBYAK_STD_UNORDERED_MULTIMAP boost::unordered_multimap
 
+#include <boost/unordered_map.hpp>
 #include <ankerl/unordered_dense.h>
-#include <unordered_map>
 
 #include <xbyak/xbyak.h>
 #include <xbyak/xbyak_util.h>
