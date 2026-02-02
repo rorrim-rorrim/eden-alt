cd667b38e39eb2743b106d6be6a597b59b7d485d
lizzie <lizzie@eden-emu.dev>
2026-02-02 06:37:19 +0000

    slug

--- tools/maxwell-spirv/reference_impl.cpp
+++ tools/maxwell-spirv/reference_impl.cpp
@@ -1,3 +1,9 @@
+// SPDX-FileCopyrightText: Copyright 2026 Eden Emulator Project
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#include "common/bit_field.h"
+#include "common/common_types.h"
+#include "spirv/unified1/spirv.hpp11"
 #include <cassert>
 #include <cstdlib>
 #include <sirit/sirit.h>
