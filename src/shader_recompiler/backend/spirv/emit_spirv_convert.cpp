827e6ca2c26f6549eba6285ebe5f41bd8aa36786
PavelBARABANOV <pavelbarabanov94@gmail.com>
2026-02-04 00:11:50 +0300

    [spirv] Fix F16 conversion on Android (skip NaN check)

--- src/shader_recompiler/backend/spirv/emit_spirv_convert.cpp
+++ src/shader_recompiler/backend/spirv/emit_spirv_convert.cpp
@@ -148,10 +148,13 @@ Id EmitConvertU32U64(EmitContext& ctx, Id value) {
 }
 
 Id EmitConvertF16F32(EmitContext& ctx, Id value) {
+#ifdef ANDROID
+    return ctx.OpFConvert(ctx.F16[1], value);
+#else
     const auto result = ctx.OpFConvert(ctx.F16[1], value);
     const auto isOverflowing = ctx.OpIsNan(ctx.U1, result);
     return ctx.OpSelect(ctx.F16[1], isOverflowing, ctx.Constant(ctx.F16[1], 0), result);
-    //return ctx.OpFConvert(ctx.F16[1], value);
+#endif
 }
 
 Id EmitConvertF32F16(EmitContext& ctx, Id value) {
