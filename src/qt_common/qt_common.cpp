f6a54b3539a5c019b4334018960f09d8cd76dbe5
crueter <crueter@eden-emu.dev>
2026-02-03 21:31:17 -0500

    Revert qt_common change
    Signed-off-by: crueter <crueter@eden-emu.dev>

--- src/qt_common/qt_common.cpp
+++ src/qt_common/qt_common.cpp
@@ -1,7 +1,6 @@
 // SPDX-FileCopyrightText: Copyright 2026 Eden Emulator Project
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-#include "frontend_common/settings_generator.h"
 #include "qt_common.h"
 #include "common/fs/fs.h"
 #include "common/fs/ryujinx_compat.h"
