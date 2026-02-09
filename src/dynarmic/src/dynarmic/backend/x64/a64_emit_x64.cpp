ccdb9b9d4c4ce81221424296ab2d3c86af86e2fe
lizzie <lizzie@eden-emu.dev>
2026-02-09 10:45:29 +0000

    [dynarmic] use bitset<32> instead of vector for ABI regset allocations HLE macro does this, may as well do it on dynarm as well :)
    Signed-off-by: lizzie <lizzie@eden-emu.dev>

--- src/dynarmic/src/dynarmic/backend/x64/a64_emit_x64.cpp
+++ src/dynarmic/src/dynarmic/backend/x64/a64_emit_x64.cpp
@@ -188,7 +188,7 @@ void A64EmitX64::ClearFastDispatchTable() {
 
 void A64EmitX64::GenTerminalHandlers() {
     // PC ends up in rcx, location_descriptor ends up in rbx
-    static_assert(std::find(ABI_ALL_CALLEE_SAVE.begin(), ABI_ALL_CALLEE_SAVE.end(), HostLoc::R12) != ABI_ALL_CALLEE_SAVE.end());
+    //static_assert(ABI_ALL_CALLEE_SAVE.test(size_t(HostLoc::R12)));
     const auto calculate_location_descriptor = [this] {
         // This calculation has to match up with A64::LocationDescriptor::UniqueHash
         // TODO: Optimization is available here based on known state of fpcr.
