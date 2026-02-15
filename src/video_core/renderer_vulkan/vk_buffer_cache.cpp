3272e1fcb51b37214534d8d82193cc3a11dce1b5
CamilleLaVey <camillelavey99@gmail.com>
2026-02-14 22:32:49 -0400

    [vulkan] adjusting BindVertexBuffer2EXT wrong calling in pipeline

--- src/video_core/renderer_vulkan/vk_buffer_cache.cpp
+++ src/video_core/renderer_vulkan/vk_buffer_cache.cpp
@@ -564,7 +564,10 @@ void BufferCacheRuntime::BindVertexBuffer(u32 index, VkBuffer buffer, u32 offset
     if (index >= device.GetMaxVertexInputBindings()) {
         return;
     }
-    if (device.IsExtExtendedDynamicStateSupported()) {
+    const bool use_dynamic_vertex_input_binding_stride =
+        device.IsExtExtendedDynamicStateSupported() &&
+        use_vertex_input_binding_stride_dynamic_state;
+    if (use_dynamic_vertex_input_binding_stride) {
         scheduler.Record([index, buffer, offset, size, stride](vk::CommandBuffer cmdbuf) {
             const VkDeviceSize vk_offset = buffer != VK_NULL_HANDLE ? offset : 0;
             const VkDeviceSize vk_size = buffer != VK_NULL_HANDLE ? size : VK_WHOLE_SIZE;
