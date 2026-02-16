24d07ab28c988bebd8c3bcb78e7330a7d526d69a
CamilleLaVey <camillelavey99@gmail.com>
2026-02-15 22:23:46 -0400

    fix build

--- src/video_core/vulkan_common/vulkan_device.h
+++ src/video_core/vulkan_common/vulkan_device.h
@@ -698,6 +698,10 @@ public:
         return extensions.conditional_rendering;
     }
 
+    bool IsExtConditionalRendering() const {
+        return IsExtConditionalRenderingSupported();
+    }
+
     bool HasTimelineSemaphore() const;
 
     /// Returns the minimum supported version of SPIR-V.
