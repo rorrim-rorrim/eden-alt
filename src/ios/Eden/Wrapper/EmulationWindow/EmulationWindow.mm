35f2fd492306f2537cd4ad41c5926dac7ea5a86e
lizzie <lizzie@eden-emu.dev>
2026-02-19 03:53:16 +0000

    loicense

--- src/ios/Eden/Wrapper/EmulationWindow/EmulationWindow.mm
+++ src/ios/Eden/Wrapper/EmulationWindow/EmulationWindow.mm
@@ -1,3 +1,6 @@
+// SPDX-FileCopyrightText: Copyright 2026 Eden Emulator Project
+// SPDX-License-Identifier: GPL-3.0-or-later
+
 //
 //  EmulationWindow.mm - Sudachi
 //  Created by Jarrod Norwell on 1/18/24.
