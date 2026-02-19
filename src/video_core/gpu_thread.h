212577450cd1c78f4d9289d61568d0a086f3d45b
lizzie <lizzie@eden-emu.dev>
2026-02-19 08:37:04 +0000

    [video_core] change MPSC->SPCS queue for synch state of GPU cmds
    Signed-off-by: lizzie <lizzie@eden-emu.dev>

--- src/video_core/gpu_thread.h
+++ src/video_core/gpu_thread.h
@@ -89,7 +89,7 @@ struct CommandDataContainer {
 
 /// Struct used to synchronize the GPU thread
 struct SynchState final {
-    using CommandQueue = Common::MPSCQueue<CommandDataContainer>;
+    using CommandQueue = Common::SPSCQueue<CommandDataContainer>;
     std::mutex write_lock;
     CommandQueue queue;
     u64 last_fence{};
