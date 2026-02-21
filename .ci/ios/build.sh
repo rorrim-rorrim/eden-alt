18b08ecfceb123d3190744cad40ac48f54f875f3
lizzie <lizzie@eden-emu.dev>
2026-02-21 23:39:48 +0000

    license

--- .ci/ios/build.sh
+++ .ci/ios/build.sh
@@ -1,5 +1,8 @@
 #!/bin/sh -ex
 
+# SPDX-FileCopyrightText: Copyright 2026 Eden Emulator Project
+# SPDX-License-Identifier: GPL-3.0-or-later
+
 WORK_DIR="$PWD"
 if [ -z "$NPROC" ]; then
     NPROC="$(nproc)"
