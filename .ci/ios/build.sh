edbc4b251f26a7c2f601a1133281aafafc8555d4
lizzie <lizzie@eden-emu.dev>
2026-02-21 23:39:01 +0000

    ios toolchain cmake

---
+++ .ci/ios/build.sh
@@ -0,0 +1,27 @@
+#!/bin/sh -ex
+
+WORK_DIR="$PWD"
+if [ -z "$NPROC" ]; then
+    NPROC="$(nproc)"
+fi
+
+cmake -G Xcode -B build \
+    -DCMAKE_TOOLCHAIN_FILE="$WORK_DIR/ios-toolchain.cmake" \
+    -DPLATFORM=OS64 \
+    -DENABLE_LIBUSB=OFF \
+    -DENABLE_UPDATE_CHECKER=OFF \
+    -DENABLE_QT=OFF \
+    -DENABLE_OPENSSL=OFF \
+    -DENABLE_WEB_SERVICE=OFF \
+    -DENABLE_CUBEB=OFF \
+    -DYUZU_ROOM=OFF \
+    -DYUZU_ROOM_STANDALONE=OFF \
+    -DYUZU_CMD=OFF \
+    -DUSE_DISCORD_PRESENCE=OFF \
+    -DYUZU_USE_EXTERNAL_FFMPEG=ON \
+    -DYUZU_USE_CPM=ON \
+    -DYUZU_USE_EXTERNAL_SDL2=ON \
+    -DCPMUTIL_FORCE_BUNDLED=ON \
+    -DCMAKE_BUILD_TYPE=Release
+
+cmake --build build -- -j${NPROC}
