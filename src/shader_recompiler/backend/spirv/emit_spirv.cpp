c028d925bc8e8f1931b04760bf17120906f07d7c
CamilleLaVey <camillelavey99@gmail.com>
2026-03-06 23:38:42 -0400

    fix build

--- src/shader_recompiler/backend/spirv/emit_spirv.cpp
+++ src/shader_recompiler/backend/spirv/emit_spirv.cpp
@@ -17,6 +17,7 @@
 #include "shader_recompiler/backend/spirv/emit_spirv_instructions.h"
 #include "shader_recompiler/backend/spirv/spirv_emit_context.h"
 #include "shader_recompiler/frontend/ir/basic_block.h"
+#include "shader_recompiler/frontend/ir/modifiers.h"
 #include "shader_recompiler/frontend/ir/program.h"
 
 namespace Shader::Backend::SPIRV {
