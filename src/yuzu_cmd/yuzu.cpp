b1de720284db09ddf146a23beda2d37cb93780bd
lizzie <lizzie@eden-emu.dev>
2026-03-16 21:38:52 +0000

    disable openssl and register user

--- src/yuzu_cmd/yuzu.cpp
+++ src/yuzu_cmd/yuzu.cpp
@@ -191,6 +191,8 @@ int main(int argc, char** argv) {
     // May prevent spurious crashes on swap handlers...
     setvbuf(stdout, nullptr, _IONBF, 0);
     setvbuf(stderr, nullptr, _IONBF, 0);
+
+    sceUserServiceInitialize(nullptr);
 #endif
 
     Common::Log::Initialize();
