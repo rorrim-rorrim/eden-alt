cef3b134e540f312b6c9ef86980d916d21d30810
lizzie <lizzie@eden-emu.dev>
2026-03-18 07:28:12 +0000

    fix cityhash

--- src/yuzu_room_standalone/main.cpp
+++ src/yuzu_room_standalone/main.cpp
@@ -1,6 +1,7 @@
-#include <iostream>
+// SPDX-FileCopyrightText: Copyright 2026 Eden Emulator Project
+// SPDX-License-Identifier: GPL-3.0-or-later
 
-using namespace std;
+#include <iostream>
 
 int main()
 {
