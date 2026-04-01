8dc786115230fae276c86e56a213b80ec6bbb818
crueter <crueter@eden-emu.dev>
2026-04-01 19:49:18 +0000

    Partial build fix
    Signed-off-by: crueter <crueter@eden-emu.dev>

---
+++ src/dynarmic/src/dynarmic/common/spin_lock_riscv64.cpp
@@ -0,0 +1,13 @@
+#include <mutex>
+
+#include "dynarmic/common/spin_lock.h"
+
+namespace Dynarmic {
+
+void SpinLock::Lock() noexcept {
+}
+
+void SpinLock::Unlock() noexcept {
+}
+
+}  // namespace Dynarmic
