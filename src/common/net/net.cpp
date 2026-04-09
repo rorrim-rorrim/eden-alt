4cd5171e1bb7f25e062f5c471df8ef6961f3ca01
crueter <crueter@eden-emu.dev>
2026-04-09 00:33:22 -0400

    Fix license headers

--- src/common/net/net.cpp
+++ src/common/net/net.cpp
@@ -1,3 +1,6 @@
+// SPDX-FileCopyrightText: Copyright 2026 Eden Emulator Project
+// SPDX-License-Identifier: GPL-3.0-or-later
+
 #include <optional>
 #include <boost/algorithm/string/classification.hpp>
 #include <boost/algorithm/string/replace.hpp>
