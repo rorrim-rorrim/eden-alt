a7aaed7585547b336d8a4b43338ab70394e53a20
lizzie <lizzie@eden-emu.dev>
2026-04-10 07:30:06 +0000

    disable openssl on httplib hdr

--- src/common/httplib.h
+++ src/common/httplib.h
@@ -3,8 +3,14 @@
 
 #pragma once
 
+// TODO: httplib port can't use OpenSSL because of package misconfigs on OO toolchain
+// right now the solution is to tell httplib that OpenSSL doesn't exist, but that's bad
+// and the issue is a bit more of how OpenSSL lacks some essential functionality
+// which is a bit annoying to reconfigure atm.
+#ifndef __OPENORBIS__
 #define CPPHTTPLIB_DISABLE_MACOSX_AUTOMATIC_ROOT_CERTIFICATES 1
 #define CPPHTTPLIB_OPENSSL_SUPPORT 1
+#endif
 
 #ifdef __GNUC__
 #pragma GCC diagnostic push
