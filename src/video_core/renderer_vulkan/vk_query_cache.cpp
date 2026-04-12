e754ed90187ebad85337b2b2866a5f794acf8f28
CamilleLaVey <camillelavey99@gmail.com>
2026-04-12 01:37:48 -0400

    [vulkan] Adjustment ResetQueryPool

--- src/video_core/renderer_vulkan/vk_query_cache.cpp
+++ src/video_core/renderer_vulkan/vk_query_cache.cpp
@@ -157,8 +157,9 @@ public:
         ReserveHostQuery();
 
         scheduler.Record([query_pool = current_query_pool,
-                                 query_index = current_bank_slot](vk::CommandBuffer cmdbuf) {
+                          query_index = current_bank_slot](vk::CommandBuffer cmdbuf) {
             const bool use_precise = Settings::IsGPULevelHigh();
+            cmdbuf.ResetQueryPool(query_pool, static_cast<u32>(query_index), 1);
             cmdbuf.BeginQuery(query_pool, static_cast<u32>(query_index),
                               use_precise ? VK_QUERY_CONTROL_PRECISE_BIT : 0);
         });
