d7c1edbde289cb252e10b9a609407b56c39ffb34
CamilleLaVey <camillelavey99@gmail.com>
2026-04-12 01:11:38 -0400

    [test] Force Synchronization of buffers

--- src/video_core/renderer_vulkan/vk_query_cache.cpp
+++ src/video_core/renderer_vulkan/vk_query_cache.cpp
@@ -1442,8 +1442,7 @@ void QueryCacheRuntime::HostConditionalRenderingCompareBCImpl(DAddr address, boo
     const u32 resolve_size = compare_to_zero ? 8 : 24;
     {
         std::scoped_lock lk(impl->buffer_cache.mutex);
-        const auto sync_info = compare_to_zero ? VideoCommon::ObtainBufferSynchronize::FullSynchronize
-                                               : VideoCommon::ObtainBufferSynchronize::NoSynchronize;
+        const auto sync_info = VideoCommon::ObtainBufferSynchronize::FullSynchronize;
         const auto post_op = VideoCommon::ObtainBufferOperation::DoNothing;
         const auto [buffer, offset] =
             impl->buffer_cache.ObtainCPUBuffer(address, resolve_size, sync_info, post_op);
