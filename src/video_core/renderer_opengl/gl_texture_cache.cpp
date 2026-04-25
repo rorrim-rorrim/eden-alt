9d75f52d6a3e0bad2bbaddea3fc9b7f4882fd7c8
CamilleLaVey <camillelavey99@gmail.com>
2026-04-25 18:17:32 -0400

    Revert "fix compiler rr"

--- src/video_core/renderer_opengl/gl_texture_cache.cpp
+++ src/video_core/renderer_opengl/gl_texture_cache.cpp
@@ -1324,7 +1324,8 @@ Sampler::Sampler(TextureCacheRuntime& runtime, const TSCEntry& config) {
     }
 }
 
-Framebuffer::Framebuffer(TextureCacheRuntime& runtime, std::span<ImageView*, NUM_RT> color_buffers, ImageView* depth_buffer, const VideoCommon::RenderTargets& key) {
+Framebuffer::Framebuffer(TextureCacheRuntime& runtime, std::span<ImageView*, NUM_RT> color_buffers,
+                         ImageView* depth_buffer, const VideoCommon::RenderTargets& key) {
     framebuffer.Create();
     GLuint handle = framebuffer.handle;
 
