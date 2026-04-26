81a1432979c73d51fb094ff5b3b1df0f1eae9cf9
lizzie <lizzie@eden-emu.dev>
2026-04-26 10:16:50 +0000

    [common] use abi::__cxa_demangle for demangling using the system's glibcxx/libc++
    Signed-off-by: lizzie <lizzie@eden-emu.dev>

--- src/common/demangle.h
+++ src/common/demangle.h
@@ -1,3 +1,6 @@
+// SPDX-FileCopyrightText: Copyright 2026 Eden Emulator Project
+// SPDX-License-Identifier: GPL-3.0-or-later
+
 // SPDX-FileCopyrightText: Copyright 2020 yuzu Emulator Project
 // SPDX-License-Identifier: GPL-2.0-or-later
 
@@ -7,6 +10,6 @@
 
 namespace Common {
 
-std::string DemangleSymbol(const std::string& mangled);
+std::string DemangleSymbol(const std::string&& mangled);
 
 } // namespace Common
