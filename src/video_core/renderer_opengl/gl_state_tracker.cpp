e7f8e314486531c6505c4e822cec03fdb197528d
lizzie <lizzie@eden-emu.dev>
2026-05-01 00:06:01 +0000

    extra

--- src/video_core/renderer_opengl/gl_state_tracker.cpp
+++ src/video_core/renderer_opengl/gl_state_tracker.cpp
@@ -204,7 +204,7 @@ void SetupDirtyMisc(Tables& tables) {
 } // Anonymous namespace
 
 void StateTracker::SetupTables(Tegra::Control::ChannelState& channel_state) {
-    auto& tables{channel_state.maxwell_3d->dirty.tables};
+    auto& tables{channel_state.payload->maxwell_3d.dirty.tables};
     SetupDirtyFlags(tables);
     SetupDirtyColorMasks(tables);
     SetupDirtyViewports(tables);
@@ -231,7 +231,7 @@ void StateTracker::SetupTables(Tegra::Control::ChannelState& channel_state) {
 }
 
 void StateTracker::ChangeChannel(Tegra::Control::ChannelState& channel_state) {
-    flags = &channel_state.maxwell_3d->dirty.flags;
+    flags = &channel_state.payload->maxwell_3d.dirty.flags;
 }
 
 void StateTracker::InvalidateState() {
