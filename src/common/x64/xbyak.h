220bd46a80a323516a77b93944863e57de8f72f8
lizzie <lizzie@eden-emu.dev>
2026-05-16 01:32:57 +0000

    fs

--- src/common/x64/xbyak.h
+++ src/common/x64/xbyak.h
@@ -7,6 +7,6 @@
 #include <ankerl/unordered_dense.h>
 #define XBYAK_STD_UNORDERED_SET ankerl::unordered_dense::set
 #define XBYAK_STD_UNORDERED_MAP ankerl::unordered_dense::map
-#define XBYAK_STD_UNORDERED_MULTIMAP std::multimap
+#define XBYAK_STD_UNORDERED_MULTIMAP std::unordered_multimap
 #include <xbyak/xbyak.h>
 #include <xbyak/xbyak_util.h>
