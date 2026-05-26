15daf67143e79a5f8b895dd96a40d552369d2b2f
lizzie <lizzie@eden-emu.dev>
2026-05-26 03:30:52 +0000

    loicense

--- src/common/container_hash.h
+++ src/common/container_hash.h
@@ -1,3 +1,6 @@
+// SPDX-FileCopyrightText: Copyright 2026 Eden Emulator Project
+// SPDX-License-Identifier: GPL-3.0-or-later
+
 // SPDX-FileCopyrightText: 2005-2014 Daniel James
 // SPDX-FileCopyrightText: 2016 Austin Appleby
 // SPDX-License-Identifier: BSL-1.0
