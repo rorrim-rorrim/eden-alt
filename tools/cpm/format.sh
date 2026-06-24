b288b2d0adf32b288ff855409715aa9f299665e2
crueter <crueter@eden-emu.dev>
2026-06-24 01:01:09 -0400

    Fix interactive add
    Signed-off-by: crueter <crueter@eden-emu.dev>

--- tools/cpm/format.sh
+++ tools/cpm/format.sh
@@ -3,7 +3,5 @@
 # SPDX-FileCopyrightText: Copyright 2026 crueter
 # SPDX-License-Identifier: LGPL-3.0-or-later
 
-file=cpmfile.json
-
-jq --indent 4 -S <"$file" >"$file".new
-mv "$file".new "$file"
+jq --indent 4 -S <cpmfile.json >cpmfile.json.new
+mv cpmfile.json.new cpmfile.json
