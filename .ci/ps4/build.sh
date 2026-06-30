e451a536bdc8d7c79ed4c0571182e97d5f400504
lizzie <lizzie@eden-emu.dev>
2026-06-30 09:07:55 +0000

    softpipe enabled mesa support

--- .ci/ps4/build.sh
+++ .ci/ps4/build.sh
@@ -5,13 +5,14 @@
 
 [ -z ${OO_PS4_TOOLCHAIN+x} ] && exit
 
+# cmake
 [ -f "ps4-toolchain.cmake" ] || cat << EOF >"ps4-toolchain.cmake"
 set(CMAKE_SYSROOT "$OO_PS4_TOOLCHAIN")
 set(CMAKE_STAGING_PREFIX "$OO_PS4_TOOLCHAIN")
 set(CMAKE_SYSTEM_NAME "OpenOrbis")
 
-set(CMAKE_C_FLAGS " -D__OPENORBIS__ -D_LIBCPP_HAS_MUSL_LIBC=1 -D_GNU_SOURCE=1 --target=x86_64-pc-freebsd12-elf -mtune=btver2 -march=btver2 -fPIC -funwind-tables -femulated-tls")
-set(CMAKE_CXX_FLAGS " -D__OPENORBIS__ -D_LIBCPP_HAS_MUSL_LIBC=1 -D_GNU_SOURCE=1 --target=x86_64-pc-freebsd12-elf -mtune=btver2 -march=btver2 -fPIC -funwind-tables -femulated-tls")
+set(CMAKE_C_FLAGS "-D__OPENORBIS__ -D_LIBCPP_HAS_MUSL_LIBC=1 -D_GNU_SOURCE=1 --target=x86_64-pc-freebsd12-elf -mtune=btver2 -march=btver2 -fPIC -funwind-tables -femulated-tls")
+set(CMAKE_CXX_FLAGS "-D__OPENORBIS__ -D_LIBCPP_HAS_MUSL_LIBC=1 -D_GNU_SOURCE=1 --target=x86_64-pc-freebsd12-elf -mtune=btver2 -march=btver2 -fPIC -funwind-tables -femulated-tls")
 
 set(CMAKE_EXE_LINKER_FLAGS "-m elf_x86_64 -pie -T $OO_PS4_TOOLCHAIN/link.x --eh-frame-hdr -L$OO_PS4_TOOLCHAIN/lib")
 set(CMAKE_C_LINK_FLAGS "-m elf_x86_64 -pie -T $OO_PS4_TOOLCHAIN/link.x --eh-frame-hdr -L$OO_PS4_TOOLCHAIN/lib")
@@ -48,7 +49,7 @@ export EXTRA_CMAKE_FLAGS=("${EXTRA_CMAKE_FLAGS[@]}" $@)
     -DENABLE_LIBUSB=OFF \
     -DENABLE_UPDATE_CHECKER=OFF \
     -DENABLE_QT=OFF \
-    -DENABLE_OPENGL=OFF \
+    -DENABLE_OPENGL=ON \
     -DENABLE_WEB_SERVICE=OFF \
     -DUSE_DISCORD_PRESENCE=OFF \
     -DCPMUTIL_FORCE_BUNDLED=ON \
