2759dd3359e87d2c8b4878ffcedd31946926a5f7
lizzie <lizzie@eden-emu.dev>
2026-07-02 17:52:42 +0000

    cstring hath to come forst?

--- src/common/dynamic_library.cpp
+++ src/common/dynamic_library.cpp
@@ -4,8 +4,8 @@
 // SPDX-FileCopyrightText: 2019 Dolphin Emulator Project
 // SPDX-License-Identifier: GPL-2.0-or-later
 
-#include <string>
 #include <cstring>
+#include <string>
 #include <utility>
 
 #include <fmt/ranges.h>
