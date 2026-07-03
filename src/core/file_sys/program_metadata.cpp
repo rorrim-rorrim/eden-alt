d2f9bf9ac04d6009bc9a98283de4b23c181eb4bb
lizzie <lizzie@eden-emu.dev>
2026-07-03 17:23:24 +0000

    extra ones

--- src/core/file_sys/program_metadata.cpp
+++ src/core/file_sys/program_metadata.cpp
@@ -181,11 +181,11 @@ const ProgramMetadata::KernelCapabilityDescriptors& ProgramMetadata::GetKernelCa
 
 void ProgramMetadata::Print() const {
     LOG_DEBUG(Service_FS, "Magic:                  {:.4}", npdm_header.magic.data());
-    LOG_DEBUG(Service_FS, "Main thread priority:   {:#02X}", npdm_header.main_thread_priority);
+    LOG_DEBUG(Service_FS, "Main thread priority:   {:#02x}", npdm_header.main_thread_priority);
     LOG_DEBUG(Service_FS, "Main thread core:       {}", npdm_header.main_thread_cpu);
     LOG_DEBUG(Service_FS, "Main thread stack size: {:#x} bytes", npdm_header.main_stack_size);
     LOG_DEBUG(Service_FS, "Process category:       {}", npdm_header.process_category);
-    LOG_DEBUG(Service_FS, "Flags:                  {:#02X}", npdm_header.flags);
+    LOG_DEBUG(Service_FS, "Flags:                  {:#02x}", npdm_header.flags);
     LOG_DEBUG(Service_FS, " > 64-bit instructions: {}",
               npdm_header.has_64_bit_instructions ? "YES" : "NO");
 
@@ -209,7 +209,7 @@ void ProgramMetadata::Print() const {
 
     // Begin ACID printing (potential perms, signed)
     LOG_DEBUG(Service_FS, "Magic:                  {:.4}", acid_header.magic.data());
-    LOG_DEBUG(Service_FS, "Flags:                  {:#02X}", acid_header.flags);
+    LOG_DEBUG(Service_FS, "Flags:                  {:#02x}", acid_header.flags);
     LOG_DEBUG(Service_FS, " > Is Retail:           {}", acid_header.production_flag ? "YES" : "NO");
     LOG_DEBUG(Service_FS, "Title ID Min:           {:#016x}", acid_header.title_id_min);
     LOG_DEBUG(Service_FS, "Title ID Max:           {:#016x}", acid_header.title_id_max);
