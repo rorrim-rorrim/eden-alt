675347569f69f7fc14a79d961f2a0223d1481200
lizzie <lizzie@eden-emu.dev>
2026-07-03 05:36:39 +0000

    [audio_core/dsp] nuke libopus, use ffmpeg for opus decoding
    Signed-off-by: lizzie <lizzie@eden-emu.dev>

--- src/audio_core/adsp/apps/opus/opus_decoder.cpp
+++ src/audio_core/adsp/apps/opus/opus_decoder.cpp
@@ -16,6 +16,7 @@
 #include "common/thread.h"
 #include "core/core.h"
 #include "core/core_timing.h"
+#include "core/hle/service/audio/errors.h"
 
 namespace AudioCore::ADSP::OpusDecoder {
 
@@ -51,11 +52,16 @@ OpusDecoder::~OpusDecoder() {
     // Shutdown the thread
     Send(Direction::DSP, Message::Shutdown);
     auto msg = Receive(Direction::Host);
-    ASSERT_MSG(msg == Message::ShutdownOK, "Expected Opus shutdown code {}, got {}",
-               Message::ShutdownOK, msg);
+    ASSERT_MSG(msg == Message::ShutdownOK, "Expected Opus shutdown code {}, got {}", Message::ShutdownOK, msg);
     main_thread.request_stop();
     main_thread.join();
     running = false;
+
+    // Must shutdown as there are AV allocations which are manual
+    for (auto& e : decode_objects)
+        e.second.Shutdown();
+    for (auto& e : ms_decode_objects)
+        e.second.Shutdown();
 }
 
 void OpusDecoder::Send(Direction dir, u32 message) {
@@ -70,7 +76,7 @@ void OpusDecoder::Init(std::stop_token rc_stop_token) {
     Common::SetCurrentThreadName("DSP_OpusDecoder_Init");
 
     if (Receive(Direction::DSP, rc_stop_token) != Message::Start) {
-        LOG_ERROR(Service_Audio, "DSP OpusDecoder failed to receive Start message. Opus initialization failed.");
+        LOG_ERROR(Audio_DSP, "DSP OpusDecoder failed to receive Start message. Opus initialization failed.");
         return;
     }
     // Main OpusDecoder thread, responsible for processing the incoming Opus packets.
@@ -82,43 +88,48 @@ void OpusDecoder::Init(std::stop_token rc_stop_token) {
             case Shutdown:
                 Send(Direction::Host, Message::ShutdownOK);
                 return;
-
             case GetWorkBufferSize: {
-                auto channel_count = static_cast<s32>(shared_memory->host_send_data[0]);
+                auto channel_count = s32(shared_memory->host_send_data[0]);
 
                 ASSERT(IsValidChannelCount(channel_count));
 
                 shared_memory->dsp_return_data[0] = OpusDecodeObject::GetWorkBufferSize(channel_count);
                 Send(Direction::Host, Message::GetWorkBufferSizeOK);
-            } break;
-
+                break;
+            }
             case InitializeDecodeObject: {
                 auto buffer = shared_memory->host_send_data[0];
                 auto buffer_size = shared_memory->host_send_data[1];
-                auto sample_rate = static_cast<s32>(shared_memory->host_send_data[2]);
-                auto channel_count = static_cast<s32>(shared_memory->host_send_data[3]);
+                auto sample_rate = s32(shared_memory->host_send_data[2]);
+                auto channel_count = s32(shared_memory->host_send_data[3]);
 
                 ASSERT(sample_rate >= 0);
                 ASSERT(IsValidChannelCount(channel_count));
                 ASSERT(buffer_size >= OpusDecodeObject::GetWorkBufferSize(channel_count));
 
-                auto& decoder_object = OpusDecodeObject::Initialize(buffer, buffer);
-                shared_memory->dsp_return_data[0] =
-                    decoder_object.InitializeDecoder(sample_rate, channel_count);
-
+                if (auto const it = decode_objects.find(buffer); it != decode_objects.end()) {
+                    it->second.Shutdown();
+                    shared_memory->dsp_return_data[0] = it->second.InitializeDecoder(sample_rate, channel_count).raw;
+                } else {
+                    OpusDecodeObject obj{};
+                    shared_memory->dsp_return_data[0] = obj.InitializeDecoder(sample_rate, channel_count).raw;
+                    decode_objects.insert_or_assign(buffer, obj);
+                }
                 Send(Direction::Host, Message::InitializeDecodeObjectOK);
-            } break;
-
+                break;
+            }
             case ShutdownDecodeObject: {
                 auto buffer = shared_memory->host_send_data[0];
-                [[maybe_unused]] auto buffer_size = shared_memory->host_send_data[1];
-
-                auto& decoder_object = OpusDecodeObject::Initialize(buffer, buffer);
-                shared_memory->dsp_return_data[0] = decoder_object.Shutdown();
-
+                //[[maybe_unused]] auto buffer_size = shared_memory->host_send_data[1];
+                if (auto const it = decode_objects.find(buffer); it != decode_objects.end()) {
+                    shared_memory->dsp_return_data[0] = it->second.Shutdown().raw;
+                } else {
+                    LOG_ERROR(Audio_DSP, "operating unregistered buffer {}", buffer);
+                    shared_memory->dsp_return_data[0] = Service::Audio::ResultLibOpusInvalidState.raw;
+                }
                 Send(Direction::Host, Message::ShutdownDecodeObjectOK);
-            } break;
-
+                break;
+            }
             case DecodeInterleaved: {
                 auto start_time = system.CoreTiming().GetGlobalTimeUs();
 
@@ -127,66 +138,59 @@ void OpusDecoder::Init(std::stop_token rc_stop_token) {
                 auto input_data_size = shared_memory->host_send_data[2];
                 auto output_data = shared_memory->host_send_data[3];
                 auto output_data_size = shared_memory->host_send_data[4];
-                auto final_range = static_cast<u32>(shared_memory->host_send_data[5]);
+                //auto final_range = static_cast<u32>(shared_memory->host_send_data[5]);
                 auto reset_requested = shared_memory->host_send_data[6];
 
                 u32 decoded_samples{0};
 
-                auto& decoder_object = OpusDecodeObject::Initialize(buffer, buffer);
-                s32 error_code{OPUS_OK};
-                if (reset_requested) {
-                    error_code = decoder_object.ResetDecoder();
+                if (auto const it = decode_objects.find(buffer); it != decode_objects.end()) {
+                    auto res = ResultSuccess;
+                    if (reset_requested)
+                        res = it->second.ResetDecoder();
+                    if (res == ResultSuccess)
+                        res = it->second.Decode(decoded_samples, output_data, output_data_size, input_data, input_data_size);
+
+                    auto end_time = system.CoreTiming().GetGlobalTimeUs();
+                    shared_memory->dsp_return_data[0] = res.raw;
+                    shared_memory->dsp_return_data[1] = decoded_samples;
+                    shared_memory->dsp_return_data[2] = (end_time - start_time).count();
+                } else {
+                    LOG_ERROR(Audio_DSP, "operating unregistered buffer {}", buffer);
+                    shared_memory->dsp_return_data[0] = Service::Audio::ResultLibOpusInvalidState.raw;
                 }
-
-                if (error_code == OPUS_OK) {
-                    error_code = decoder_object.Decode(decoded_samples, output_data, output_data_size,
-                                                    input_data, input_data_size);
-                }
-
-                if (error_code == OPUS_OK) {
-                    if (final_range && decoder_object.GetFinalRange() != final_range) {
-                        error_code = OPUS_INVALID_PACKET;
-                    }
-                }
-
-                auto end_time = system.CoreTiming().GetGlobalTimeUs();
-                shared_memory->dsp_return_data[0] = error_code;
-                shared_memory->dsp_return_data[1] = decoded_samples;
-                shared_memory->dsp_return_data[2] = (end_time - start_time).count();
-
                 Send(Direction::Host, Message::DecodeInterleavedOK);
-            } break;
-
+                break;
+            }
             case MapMemory: {
                 [[maybe_unused]] auto buffer = shared_memory->host_send_data[0];
                 [[maybe_unused]] auto buffer_size = shared_memory->host_send_data[1];
                 Send(Direction::Host, Message::MapMemoryOK);
-            } break;
-
+                break;
+            }
             case UnmapMemory: {
                 [[maybe_unused]] auto buffer = shared_memory->host_send_data[0];
                 [[maybe_unused]] auto buffer_size = shared_memory->host_send_data[1];
                 Send(Direction::Host, Message::UnmapMemoryOK);
-            } break;
-
+                break;
+            }
             case GetWorkBufferSizeForMultiStream: {
-                auto total_stream_count = static_cast<s32>(shared_memory->host_send_data[0]);
-                auto stereo_stream_count = static_cast<s32>(shared_memory->host_send_data[1]);
+                auto total_stream_count = s32(shared_memory->host_send_data[0]);
+                auto stereo_stream_count = s32(shared_memory->host_send_data[1]);
 
                 ASSERT(IsValidMultiStreamStreamCounts(total_stream_count, stereo_stream_count));
 
                 shared_memory->dsp_return_data[0] = OpusMultiStreamDecodeObject::GetWorkBufferSize(
                     total_stream_count, stereo_stream_count);
                 Send(Direction::Host, Message::GetWorkBufferSizeForMultiStreamOK);
-            } break;
-
+                break;
+            }
             case InitializeMultiStreamDecodeObject: {
                 auto buffer = shared_memory->host_send_data[0];
                 auto buffer_size = shared_memory->host_send_data[1];
-                auto sample_rate = static_cast<s32>(shared_memory->host_send_data[2]);
-                auto channel_count = static_cast<s32>(shared_memory->host_send_data[3]);
-                auto total_stream_count = static_cast<s32>(shared_memory->host_send_data[4]);
-                auto stereo_stream_count = static_cast<s32>(shared_memory->host_send_data[5]);
+                auto sample_rate = s32(shared_memory->host_send_data[2]);
+                auto channel_count = s32(shared_memory->host_send_data[3]);
+                auto total_stream_count = s32(shared_memory->host_send_data[4]);
+                auto stereo_stream_count = s32(shared_memory->host_send_data[5]);
                 // Nintendo seem to have a bug here, they try to use &host_send_data[6] for the channel
                 // mappings, but [6] is never set, and there is not enough room in the argument data for
                 // more than 40 channels, when 255 are possible.
@@ -197,26 +201,31 @@ void OpusDecoder::Init(std::stop_token rc_stop_token) {
 
                 ASSERT(IsValidMultiStreamStreamCounts(total_stream_count, stereo_stream_count));
                 ASSERT(sample_rate >= 0);
-                ASSERT(buffer_size >= OpusMultiStreamDecodeObject::GetWorkBufferSize(
-                                        total_stream_count, stereo_stream_count));
+                ASSERT(buffer_size >= OpusMultiStreamDecodeObject::GetWorkBufferSize(total_stream_count, stereo_stream_count));
 
-                auto& decoder_object = OpusMultiStreamDecodeObject::Initialize(buffer, buffer);
-                shared_memory->dsp_return_data[0] = decoder_object.InitializeDecoder(
-                    sample_rate, total_stream_count, channel_count, stereo_stream_count, mappings);
-
+                if (auto const it = ms_decode_objects.find(buffer); it != ms_decode_objects.end()) {
+                    it->second.Shutdown();
+                    shared_memory->dsp_return_data[0] = it->second.InitializeDecoder(sample_rate, total_stream_count, channel_count, stereo_stream_count, mappings).raw;
+                } else {
+                    OpusMultiStreamDecodeObject obj{};
+                    shared_memory->dsp_return_data[0] = obj.InitializeDecoder(sample_rate, total_stream_count, channel_count, stereo_stream_count, mappings).raw;
+                    ms_decode_objects.insert_or_assign(buffer, obj);
+                }
                 Send(Direction::Host, Message::InitializeMultiStreamDecodeObjectOK);
-            } break;
-
+                break;
+            }
             case ShutdownMultiStreamDecodeObject: {
                 auto buffer = shared_memory->host_send_data[0];
-                [[maybe_unused]] auto buffer_size = shared_memory->host_send_data[1];
-
-                auto& decoder_object = OpusMultiStreamDecodeObject::Initialize(buffer, buffer);
-                shared_memory->dsp_return_data[0] = decoder_object.Shutdown();
-
+                //[[maybe_unused]] auto buffer_size = shared_memory->host_send_data[1];
+                if (auto const it = ms_decode_objects.find(buffer); it != ms_decode_objects.end()) {
+                    shared_memory->dsp_return_data[0] = it->second.Shutdown().raw;
+                } else {
+                    LOG_ERROR(Audio_DSP, "operating unregistered buffer {}", buffer);
+                    shared_memory->dsp_return_data[0] = Service::Audio::ResultLibOpusInvalidState.raw;
+                }
                 Send(Direction::Host, Message::ShutdownMultiStreamDecodeObjectOK);
-            } break;
-
+                break;
+            }
             case DecodeInterleavedForMultiStream: {
                 auto start_time = system.CoreTiming().GetGlobalTimeUs();
 
@@ -225,38 +234,31 @@ void OpusDecoder::Init(std::stop_token rc_stop_token) {
                 auto input_data_size = shared_memory->host_send_data[2];
                 auto output_data = shared_memory->host_send_data[3];
                 auto output_data_size = shared_memory->host_send_data[4];
-                auto final_range = static_cast<u32>(shared_memory->host_send_data[5]);
+                //auto final_range = static_cast<u32>(shared_memory->host_send_data[5]);
                 auto reset_requested = shared_memory->host_send_data[6];
 
                 u32 decoded_samples{0};
 
-                auto& decoder_object = OpusMultiStreamDecodeObject::Initialize(buffer, buffer);
-                s32 error_code{OPUS_OK};
-                if (reset_requested) {
-                    error_code = decoder_object.ResetDecoder();
+                if (auto const it = ms_decode_objects.find(buffer); it != ms_decode_objects.end()) {
+                    auto res = ResultSuccess;
+                    if (reset_requested)
+                        res = it->second.ResetDecoder();
+                    if (res == ResultSuccess)
+                        res = it->second.Decode(decoded_samples, output_data, output_data_size, input_data, input_data_size);
+
+                    auto end_time = system.CoreTiming().GetGlobalTimeUs();
+                    shared_memory->dsp_return_data[0] = res.raw;
+                    shared_memory->dsp_return_data[1] = decoded_samples;
+                    shared_memory->dsp_return_data[2] = (end_time - start_time).count();
+                } else {
+                    LOG_ERROR(Audio_DSP, "operating unregistered buffer {}", buffer);
+                    shared_memory->dsp_return_data[0] = Service::Audio::ResultLibOpusInvalidState.raw;
                 }
-
-                if (error_code == OPUS_OK) {
-                    error_code = decoder_object.Decode(decoded_samples, output_data, output_data_size,
-                                                    input_data, input_data_size);
-                }
-
-                if (error_code == OPUS_OK) {
-                    if (final_range && decoder_object.GetFinalRange() != final_range) {
-                        error_code = OPUS_INVALID_PACKET;
-                    }
-                }
-
-                auto end_time = system.CoreTiming().GetGlobalTimeUs();
-                shared_memory->dsp_return_data[0] = error_code;
-                shared_memory->dsp_return_data[1] = decoded_samples;
-                shared_memory->dsp_return_data[2] = (end_time - start_time).count();
-
                 Send(Direction::Host, Message::DecodeInterleavedForMultiStreamOK);
-            } break;
-
+                break;
+            }
             default:
-                LOG_ERROR(Service_Audio, "Invalid OpusDecoder command {}", msg);
+                LOG_ERROR(Audio_DSP, "Invalid OpusDecoder command {}", msg);
                 continue;
             }
         }
--- src/audio_core/opus/hardware_opus.h
+++ src/audio_core/opus/hardware_opus.h
@@ -1,11 +1,12 @@
+// SPDX-FileCopyrightText: Copyright 2026 Eden Emulator Project
+// SPDX-License-Identifier: GPL-3.0-or-later
+
 // SPDX-FileCopyrightText: Copyright 2023 yuzu Emulator Project
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #pragma once
 
 #include <mutex>
-#include <opus.h>
-
 #include "audio_core/adsp/apps/opus/opus_decoder.h"
 #include "audio_core/adsp/apps/opus/shared_memory.h"
 #include "audio_core/adsp/mailbox.h"
