531dc0957ef49cf519f54d1414fc2154eda02a91
lizzie <lizzie@eden-emu.dev>
2026-07-03 18:13:05 +0000

    forgot this one oops!

--- src/core/hle/service/psc/time/steady_clock.cpp
+++ src/core/hle/service/psc/time/steady_clock.cpp
@@ -13,10 +13,11 @@ namespace Service::PSC::Time {
 
 SteadyClock::SteadyClock(Core::System& system_, std::shared_ptr<TimeManager> manager,
                          bool can_write_steady_clock, bool can_write_uninitialized_clock)
-    : ServiceFramework{system_, "ISteadyClock"}, m_system{system},
-      m_clock_core{manager->m_standard_steady_clock},
-      m_can_write_steady_clock{can_write_steady_clock}, m_can_write_uninitialized_clock{
-                                                            can_write_uninitialized_clock} {
+    : ServiceFramework{system_, "ISteadyClock"}
+    , m_clock_core{manager->m_standard_steady_clock}
+    , m_can_write_steady_clock{can_write_steady_clock}
+    , m_can_write_uninitialized_clock{can_write_uninitialized_clock}
+{
     // clang-format off
          static const FunctionInfo functions[] = {
         {0, D<&SteadyClock::GetCurrentTimePoint>, "GetCurrentTimePoint"},
