90224faa54eff522650a2aa1b317f17451f1a980
crueter <crueter@eden-emu.dev>
2026-07-06 14:31:00 -0400

    delete old scripts
    Signed-off-by: crueter <crueter@eden-emu.dev>

--- tools/cpm/format.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh -e
-
-# SPDX-FileCopyrightText: Copyright 2026 crueter
-# SPDX-License-Identifier: LGPL-3.0-or-later
-
-jq --indent 4 -S <cpmfile.json >cpmfile.json.new
-mv cpmfile.json.new cpmfile.json
-
-# TODO: Run some sanity checks e.g. patches exist, etc.
